ec0b3061dfe6ef33dda81a056de5df2352ba2dde
Francois Berenger <unixjunkie@sdf.org>
2018-10-17 15:29:00 +0900

    updated test

--- test.sh
+++ test.sh
@@ -3,8 +3,9 @@
 set -x # DEBUG
 
 # encoding an SDF or a SMILES file is the same
-diff <(./type_atoms.py data/caffeine.sdf) <(./type_atoms.py data/caffeine.smi)
+diff <(./bin/type_atoms.py data/caffeine.sdf) \
+     <(./bin/type_atoms.py data/caffeine.smi)
 
 # ph4 features are the same than the ones extracted by ShowFeats.py
 # (that were checked by hand and stored in a reference file)
-diff <(./ph4_type_atoms.py data/caffeine.sdf) data/caffeine_feats.ref
+diff <(./bin/ph4_type_atoms.py data/caffeine.sdf) data/caffeine_feats.ref
