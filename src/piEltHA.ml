858b585585b56ff6c11fbed84b2a69fb1a323ea2
Francois Berenger <unixjunkie@sdf.org>
2018-10-23 14:18:26 +0900

    PiEltHA: to_string consistent with the input style (no leading/trailing 0s)

--- src/piEltHA.ml
+++ src/piEltHA.ml
@@ -1,14 +1,25 @@
 
-(* atom type used by the atom pairs molecular encoding; Cf.
+(* atom type used by the atom pairs molecular encoding
 
+   Cf.
    Carhart, R. E., Smith, D. H., & Venkataraghavan, R. (1985).
    Atom pairs as molecular features in structure-activity studies:
    definition and applications.
    Journal of Chemical Information and Computer Sciences, 25(2), 64-73. *)
 
+open Printf
+
 type t = { pi: int; (* number of pi electrons *)
            elt: string; (* element symbol *)
-           nbHA: int } (* number of heavy atom neighbors *)
+           nbHA: int } (* number of connected heavy atoms (direct neighbors) *)
+
+let dummy = { pi = -1; elt = "KO"; nbHA = -1 }
+
+let to_string x =
+  sprintf "%s%s%s"
+    (if x.pi = 0 then "" else Utls.string_of_digit x.pi)
+    x.elt
+    (if x.nbHA = 0 then "" else Utls.string_of_digit x.nbHA)
 
 exception Break
 
@@ -20,7 +31,7 @@ let of_string s =
     begin
       try
         for i = 0 to n - 1 do
-          if BatChar.is_digit s.(i) then incr count
+          if BatChar.is_digit s.[i] then incr count
           else raise Break
         done
       with Break -> ()
@@ -30,7 +41,7 @@ let of_string s =
   assert(nb_leading_digits == 0 || nb_leading_digits == 1);
   let pi =
     if nb_leading_digits = 0 then 0
-    else Utls.int_of_digit_char s.(0) in
+    else Utls.int_of_digit_char s.[0] in
   (* extract elt part: all non digit chars of the string *)
   let elt = BatString.filter (fun c -> not (BatChar.is_digit c)) s in
   (* extract nbHA part: trailing digits, if any *)
@@ -39,7 +50,7 @@ let of_string s =
     begin
       try
         for i = n - 1 downto 0 do
-          if BatChar.is_digit s.(i) then incr count
+          if BatChar.is_digit s.[i] then incr count
           else raise Break
         done
       with Break -> ()
@@ -48,5 +59,5 @@ let of_string s =
   assert(nb_trailing_digits == 0 || nb_trailing_digits == 1);
   let nbHA =
     if nb_trailing_digits = 0 then 0
-    else Utls.int_of_digit_char s.(n - 1) in
+    else Utls.int_of_digit_char s.[n - 1] in
   { pi; elt; nbHA }
