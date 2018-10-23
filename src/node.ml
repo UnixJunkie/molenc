ed55bf13c10d573e8a383961748b5c7dd0187e22
Francois Berenger <unixjunkie@sdf.org>
2018-10-23 15:06:02 +0900

    Node: from Sybyl.t to PiEltHA.t

--- src/node.ml
+++ src/node.ml
@@ -1,23 +1,24 @@
 open Printf
 
-type t = { typ: Sybyl.t; (* atom type *)
-           succs: IntSet.t } (* indexes of its direct successors in the molecular graph (it is bonded to them) *)
+type t = { typ: PiEltHA.t ; (* atom type *)
+           succs: IntSet.t } (* indexes of its direct successors
+                                in the molecular graph (it is bonded to them) *)
 
 let create typ succs =
   { typ; succs }
 
-let dummy = create Sybyl.Du IntSet.empty
+let dummy = create PiEltHA.dummy IntSet.empty
 
 let add_succ (n: t) (succ: int): t =
   create n.typ (IntSet.add succ n.succs)
 
 let to_string (n: t): string =
   sprintf "%s %s"
-    (Sybyl.to_string n.typ)
+    (PiEltHA.to_string n.typ)
     (IntSet.to_string n.succs)
 
 let get_succs (n: t): IntSet.t =
   n.succs
 
-let get_typ (n: t): Sybyl.t =
+let get_typ (n: t): PiEltHA.t =
   n.typ
