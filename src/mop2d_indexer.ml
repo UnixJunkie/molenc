96f3c256a118e7f7b7efa790b65672313957ceea
Francois Berenger <unixjunkie@sdf.org>
2018-10-23 15:04:41 +0900

    Mop2d_indexer: from mol2 to atom-pairs (input file)

--- src/mop2d_indexer.ml
+++ src/mop2d_indexer.ml
@@ -12,19 +12,18 @@ let main () =
   let argc, args = CLI.init () in
   if argc = 1 then
     (eprintf "usage:\n\
-              %s -i molecules.mol2 -r max_radius -o output.idx\n"
+              %s -i molecules.txt -r max_radius -o output.idx\n"
        Sys.argv.(0);
      exit 1);
   let input_fn = CLI.get_string ["-i"] args in
   let output_fn = CLI.get_string ["-o"] args in
   let radius = CLI.get_int ["-r"] args in
-  assert(BatString.ends_with input_fn ".mol2");
   Utls.with_infile_outfile input_fn output_fn (fun input output ->
       let counter = ref 0 in
       let ht = Ht.create 11 in
       try
         while true do
-          let m = Mol2.read_one counter input in
+          let m = Ap_types.read_one counter input in
           if !counter mod 1000 = 0 then
             eprintf "%d molecules seen\r%!" !counter; (* user feedback *)
           let mop2d = Mini_mol.mop2d_encode radius m in
