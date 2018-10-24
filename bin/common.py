2f345310921d5f503c2bf089b811eacbf691796c
Francois Berenger <unixjunkie@sdf.org>
2018-10-24 14:53:24 +0900

    bin/common.py: added print_distance_matrix

--- bin/common.py
+++ bin/common.py
@@ -1,4 +1,10 @@
 
+import numpy
+import rdkit
+from rdkit import Chem
+
+from __future__ import print_function
+
 # in a bond: atom with lowest index first
 # in a list of bonds: bond with lowest first atom index first
 def order_bonds_canonically(bonds):
@@ -17,3 +23,17 @@ def print_bond(b):
 def print_bonds(mol):
     print("#bonds:%d" % mol.GetNumBonds())
     iterate(print_bond, order_bonds_canonically(mol.GetBonds()))
+
+def print_distance_matrix(mol):
+    mat = Chem.GetDistanceMatrix(mol)
+    diam = numpy.max(mat)
+    print("#diameter=%d" % diam)
+    nb_atoms = mol.GetNumAtoms()
+    for i in range(nb_atoms):
+        for j in range(nb_atoms):
+            x = mat[i][j]
+            if j == 0:
+                print("%d" % x, end='')
+            else:
+                print(" %d" % x, end='')
+        print("") # newline
