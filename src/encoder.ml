0e8802c494366206860700ac6e760da5ab29307a
Francois Berenger <unixjunkie@sdf.org>
2019-03-20 17:20:00 +0900

    Encoder: can do multi-scale

--- src/encoder.ml
+++ src/encoder.ml
@@ -1,6 +1,8 @@
 
-(* output atom environments; sorted by decreasing frequency given
-   a file containing molecules *)
+(* molecular encoder: a molecule is a list of atom environments.
+   canonicalization is done by sorting (atoms in an environment
+   as well as the list of environments that constitutes
+   the entirely encoded molecule) *)
 
 open Printf
 
@@ -15,26 +17,34 @@ let main () =
   let argc, args = CLI.init () in
   if argc = 1 then
     (eprintf "usage:\n\
-              %s -i molecules.{types|ph4} -r max_radius -o output.idx\n"
+              %s -i molecules.{types|ph4} -r {radius|srad:frad} -o output.idx\n\
+              -i <filename>: where to read molecules from\n\
+              -r {<int>|<int>:<int>}: encoding radius or radii range\n\
+              -o <filename>: where to write encoded molecules\n"
        Sys.argv.(0);
      exit 1);
   let input_fn = CLI.get_string ["-i"] args in
   assert(BatString.ends_with input_fn ".types" ||
          BatString.ends_with input_fn ".ph4");
   let output_fn = CLI.get_string ["-o"] args in
-  let radius = CLI.get_int ["-r"] args in
+  let scale = Scale.of_string (CLI.get_string ["-r"] args) in
+  let radii = Scale.to_list scale in
   Utls.with_infile_outfile input_fn output_fn (fun input output ->
-      fprintf output "#radius=%d\n" radius;
+      fprintf output "#radius=%s\n" (Scale.to_string scale);
       let counter = ref 0 in
       try
         while true do
           let m = Ap_types.read_one counter input in
+          let name = Mini_mol.get_name m in
           if !counter mod 1000 = 0 then
             eprintf "%d molecules seen\r%!" !counter; (* user feedback *)
-          let envs = Mini_mol.encode radius m in
-          L.iter (fun (env, count) ->
-              fprintf output "%s %d\n" (Atom_env.to_string env) count
-            ) envs
+          fprintf output "#%s\n" name;
+          L.iter (fun radius ->
+              let envs = Mini_mol.encode radius m in
+              L.iter (fun (env, count) ->
+                  fprintf output "%s %d\n" (Atom_env.to_string env) count
+                ) envs
+            ) radii
         done
       with End_of_file ->
         Log.info "read %d from %s" !counter input_fn
