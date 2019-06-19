078d9def64a3a2973580b9d426752801d0e74c7a
Francois Berenger <unixjunkie@sdf.org>
2019-06-19 16:42:02 +0900

    molenc.sh: more user friendly

--- bin/molenc.sh
+++ bin/molenc.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
 
-set -x
+# set -x # DEBUG
+
+# check input_fn and output_fn are passed
+if [ "$#" -ne 2 ]; then
+    echo "usage: molenc.sh input.smi output.txt"
+    exit 1
+fi
 
 input=$1
 output=$2
 
-# FBR: check there are two params; print usage if not
-
+std_log=$input'.std_log'
 tmp=`mktemp`
 tmp_smi=$tmp'_std.smi'
 tmp_types=$tmp'_std.types'
 tmp_enc=$tmp'_std.enc'
 
-(standardiser -i $input -o $tmp_smi 2>&1) > $input'.std_log'
+(standardiser -i $input -o $tmp_smi 2>&1) > $std_log
 molenc_type_atoms.py $tmp_smi > $tmp_types
 molenc_e -i $tmp_types -r 0:1 -o $tmp_enc
 molenc_d -i $tmp_enc -o $output
+
+# cleanup
+rm -f $std_log $tmp $tmp_smi $tmp_types $tmp_enc
