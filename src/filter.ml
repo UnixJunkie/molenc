b50e564ce30b1c22b149d3ff5a09c2f1ad8db3d3
Francois Berenger <unixjunkie@sdf.org>
2019-07-19 17:51:35 +0900

    Filter: WIP

--- src/filter.ml
+++ src/filter.ml
@@ -1,56 +1,49 @@
 (* Encoded molecules filtering and diversity selection.
    Functionalities:
-   - remove training set molecules (or anything too near) from a selection of
-     (top scoring) molecules
-   - enforce diversity in a set of compounds *)
+   - remove training set molecules (or anything too near) from a
+     database of molecules
+     _OR_
+   - enforce diversity in a database of molecules *)
 
 open Printf
 
 module CLI = Minicli.CLI
-module L = MyList
-module Ht = BatHashtbl
-module StringSet = BatSet.String
+
+type mode = Filter of string (* file from where to read molecules to exclude *)
+          | Diversify
 
 let main () =
   Log.(set_log_level INFO);
   Log.color_on ();
   let argc, args = CLI.init () in
   if argc = 1 then
-    (eprintf "usage:\n\
-              %s -i molecules.{types|ph4} -r {radius|srad:frad} -o output.idx\n\
-              -i <filename>: where to read molecules from\n\
-              -r {<int>|<int>:<int>}: encoding radius or radii range\n\
-              -o <filename>: where to write encoded molecules\n"
-       Sys.argv.(0);
-     exit 1);
+    begin
+      eprintf "usage:\n\
+               %s\n \
+               -i <filename>: molecules to filter (database)\n  \
+               -o <filename>: output file\n  \
+               [-t <float>]: Tanimoto threshold (default=1.0)\n  \
+               [-e <filename>]: molecules to exclude from the ones to filter\n"
+        Sys.argv.(0);
+      exit 1
+    end;
   let input_fn = CLI.get_string ["-i"] args in
-  (* assert(BatString.ends_with input_fn ".types" ||
-   *        BatString.ends_with input_fn ".ph4"); *)
   let output_fn = CLI.get_string ["-o"] args in
-  let scale = Scale.of_string (CLI.get_string ["-r"] args) in
-  let radii = Scale.to_list scale in
+  let threshold = CLI.get_float_def ["-t"] args 1.0 in
+  let mode = match CLI.get_string_opt ["-e"] args with
+    | None -> Diversify
+    | Some fn -> Filter fn in
+  CLI.finalize ();
   Utls.with_infile_outfile input_fn output_fn (fun input output ->
-      fprintf output "#radius=%s\n" (Scale.to_string scale);
       let counter = ref 0 in
       try
-        while true do
-          let m = Ap_types.read_one counter input in
-          if !counter mod 1000 = 0 then
-            eprintf "molecules seen: %d\r%!" !counter; (* user feedback *)
-          let name = Mini_mol.get_name m in
-          let seen_envs = Ht.create 11 in
-          fprintf output "#%s\n" name;
-          L.iter (fun radius ->
-              let envs = Mini_mol.encode radius m in
-              L.iter (fun (env, count) ->
-                  (* only output envs that were not already encountered
-                     at lower radius *)
-                  if not (Ht.mem seen_envs env) then
-                    (fprintf output "%s %d\n" (Atom_env.to_string env) count;
-                     Ht.add seen_envs env ())
-                ) envs
-            ) radii
-        done
+        begin match mode with
+        | Diversify -> assert(false)
+        | Filter train_fn ->
+          while true do
+            failwith "not implemented yet"
+          done
+        end
       with End_of_file ->
         Log.info "read %d from %s" !counter input_fn
     )
