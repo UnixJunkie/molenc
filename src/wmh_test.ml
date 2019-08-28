6d5bf2fe6934ef4a783bae9183c4b8a6bfaf4968
Francois Berenger <unixjunkie@sdf.org>
2019-08-28 15:19:47 +0900

    indent

--- src/wmh_test.ml
+++ src/wmh_test.ml
@@ -50,40 +50,40 @@ let main () =
   L.iter (fun k ->
       let data_fn = sprintf "k_%02d.data" k in
       Utls.with_out_file data_fn (fun out ->
-      (* hash them (and compute hashing rate) *)
-      let seeds = WMH.get_seeds k in
-      let rands = WMH.gen_rands seeds rand_bound in
-      Gc.full_major ();
-      let dt0, hashes = Utls.time_it (fun () ->
-          A.map (WMH.hash rands idx2feat feat2acc_bound) dense_fingerprints
-        ) in
-      Log.info "k: %d hashing-rate: %.2f" k (float n /. dt0);
-      (* compute estimated tani for the same pairs (and compute scoring rate) *)
-      Gc.full_major ();
-      Random.init 12345; (* seed PRNG *)
-      let dt2, est_dists = Utls.time_it (fun () ->
-          let res = A.make n 0.0 in
-          for i = 0 to n - 1 do
-            let i1 = Random.int n in
-            let i2 = Random.int n in
-            let m1 = A.get hashes i1 in
-            let m2 = A.get hashes i2 in
-            let tani = WMH.estimate_jaccard m1 m2 in
-            A.set res i tani
-          done;
-          res) in
-      A.iteri (fun i exact_dist ->
-          fprintf out "%f %f\n" exact_dist est_dists.(i)
-        ) dists;
-      let est_tani_rate = (float n) /. dt2 in
-      (if est_tani_rate <= tani_rate then Log.warn
-      else Log.info) "k: %d est-Tani-rate: %.2f" k est_tani_rate;
-      (* output maximum Tani error *)
-      let diffs = A.map2 (fun d1 d2 -> abs_float (d1 -. d2)) dists est_dists in
-      let max_abs_error = A.max diffs in
-      let avg_abs_error = A.favg diffs in
-      Log.info "k: %d max-abs-error: %.2f avg-abs-error: %.2f"
-        k max_abs_error avg_abs_error
+          (* hash them (and compute hashing rate) *)
+          let seeds = WMH.get_seeds k in
+          let rands = WMH.gen_rands seeds rand_bound in
+          Gc.full_major ();
+          let dt0, hashes = Utls.time_it (fun () ->
+              A.map (WMH.hash rands idx2feat feat2acc_bound) dense_fingerprints
+            ) in
+          Log.info "k: %d hashing-rate: %.2f" k (float n /. dt0);
+          (* compute estimated tani for the same pairs (and compute scoring rate) *)
+          Gc.full_major ();
+          Random.init 12345; (* seed PRNG *)
+          let dt2, est_dists = Utls.time_it (fun () ->
+              let res = A.make n 0.0 in
+              for i = 0 to n - 1 do
+                let i1 = Random.int n in
+                let i2 = Random.int n in
+                let m1 = A.get hashes i1 in
+                let m2 = A.get hashes i2 in
+                let tani = WMH.estimate_jaccard m1 m2 in
+                A.set res i tani
+              done;
+              res) in
+          A.iteri (fun i exact_dist ->
+              fprintf out "%f %f\n" exact_dist est_dists.(i)
+            ) dists;
+          let est_tani_rate = (float n) /. dt2 in
+          (if est_tani_rate <= tani_rate then Log.warn
+           else Log.info) "k: %d est-Tani-rate: %.2f" k est_tani_rate;
+          (* output maximum Tani error *)
+          let diffs = A.map2 (fun d1 d2 -> abs_float (d1 -. d2)) dists est_dists in
+          let max_abs_error = A.max diffs in
+          let avg_abs_error = A.favg diffs in
+          Log.info "k: %d max-abs-error: %.2f avg-abs-error: %.2f"
+            k max_abs_error avg_abs_error
         )
     ) ks
 
