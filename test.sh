604b5f200319d25d81a37fe7a8b4ceb29ff99fd3
Francois Berenger <unixjunkie@sdf.org>
2019-09-09 12:53:21 +0900

    test.sh: repaired regression tests
    also added a test for the pubchem_decoder

--- test.sh
+++ test.sh
@@ -4,9 +4,11 @@
 
 # encoding an SDF or a SMILES file is the same
 # and it is the one we expect
-diff <(./bin/type_atoms.py data/caff_coca.sdf) data/caff_coca_types.ref
-diff <(./bin/type_atoms.py data/caff_coca.smi) data/caff_coca_types.ref
+diff <(./bin/molenc_type_atoms.py data/caff_coca.sdf) data/caff_coca_types.ref
+diff <(./bin/molenc_type_atoms.py data/caff_coca.smi) data/caff_coca_types.ref
 
 # ph4 features are the same than the ones extracted by ShowFeats.py
 # (that were checked by hand and stored in a reference file)
-diff <(./bin/ph4_type_atoms.py data/caff_coca.sdf) data/caff_coca_feats.ref
+diff <(./bin/molenc_ph4_type_atoms.py data/caff_coca.sdf) data/caff_coca_feats.ref
+
+diff <(_build/default/src/pubchem_decoder.exe -i data/test_in.pbc -o /dev/stdout) data/test_out.ref
