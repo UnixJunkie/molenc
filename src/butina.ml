a1006be49921fa52cf4057230c40bffd5a345c52
Francois Berenger <unixjunkie@sdf.org>
2019-09-11 17:45:18 +0900

    Butina: WIP

--- src/butina.ml
+++ src/butina.ml
@@ -24,54 +24,60 @@ module FpMol = Molenc.FpMol
 module BST = Bst.Bisec_tree.Make (FpMol)
 module Ht = Hashtbl
 module L = BatList
+module StringSet = BatSet.String
 module Utls = Molenc.Utls
 
 let verbose = ref false
 
-let butina tani_t molecules =
-  let n = L.length molecules in
-  let clusters = Ht.create n in
-  let clustered = Ht.create n in
+let sort_by_decr_density clusters =
+  L.sort (fun (_c1, _m1, s1) (_c2, _m2, s2) ->
+      BatInt.compare s2 s1
+    ) clusters
+
+let butina nprocs tani_t molecules =
   let dist_t = 1.0 -. tani_t in
-  let cid = ref 0 in
-  let rec loop mols =
-    match mols with
-    | [] -> ()
-    | _ ->
-      (* assign neighborhood density to each mol *)
-      let bst = BST.(create 20 One_band (Array.of_list mols)) in
-      (* FBR: detect and remove all singletons here *)
-      let mol_densities =
-        (* FBR: this should use parmap *)
-        L.map (fun mol ->
-            let neighbors = BST.neighbors mol dist_t bst in
-            (mol, neighbors, L.length neighbors)
-          ) mols in
-      let highest_density_first =
-        L.sort (fun (_m1, _mn1, n1) (_m2, _mn2, n2) ->
-            BatInt.compare n2 n1
-          ) mol_densities in
-      match highest_density_first with
-      | [] -> assert(false)
-      | (center, members, size) :: centers ->
-        begin
-          Ht.add clusters center members;
-          (* mark members as clustered *)
-          L.iter (fun m ->
-              Ht.add clustered m ()
-            ) members;
-          Log.info "cluster: %d size: %d clustered: %d"
-            !cid size (Ht.length clustered);
-          incr cid;
-          let unclustered =
-            L.fold_left (fun acc (x, _neighbs, _n) ->
-                if Ht.mem clustered x then acc
-                else x :: acc
-              ) [] centers in
-          loop unclustered
-        end in
-  loop molecules;
-  clusters
+  let rec loop clusters clustered = function
+    | [] -> L.rev clusters
+    | (center, members, size) :: rest ->
+      let clusters' = (center, members, size) :: clusters in
+      let clustered' = StringSet.union clustered members in
+      (* update remaining clusters *)
+      let to_cluster =
+        L.filter (fun (c, _m, _s) ->
+            not (StringSet.mem (FpMol.get_name c) clustered')
+          ) rest in
+      (* update their members and sizes *)
+      let to_cluster' =
+        L.map (fun (c, m, _s) ->
+            let m' = StringSet.diff m clustered' in
+            (c, m', StringSet.cardinal m')
+          ) to_cluster in
+      let to_cluster'' = sort_by_decr_density to_cluster' in
+      loop clusters' clustered' to_cluster'' in
+  (* density around each mol *)
+  let bst = BST.(create 1 Two_bands (Array.of_list molecules)) in
+  let mol_densities =
+    Parmap.parmap ~ncores:nprocs ~chunksize:1 (fun mol ->
+        let neighbors = BST.neighbors mol dist_t bst in
+        let nb_neighbs = L.length neighbors in
+        let neighbor_names = StringSet.of_list (L.map FpMol.get_name neighbors) in
+        (mol, neighbor_names, nb_neighbs)
+      ) (Parmap.L molecules) in
+  (* isolated molecules (singleton clusters) are handled here *)
+  let singletons, others = L.partition (fun (_c, _m, s) -> s = 1) mol_densities in
+  let clustered =
+    List.fold_left (fun acc (c, _m, _s) ->
+        StringSet.add (FpMol.get_name c) acc
+      ) StringSet.empty singletons in
+  (* update members *)
+  let to_cluster =
+    L.map (fun (c, m, _s) ->
+        let m' = StringSet.diff m clustered in
+        (c, m', StringSet.cardinal m')
+      ) others in
+  let highest_density_first = sort_by_decr_density to_cluster in
+  let clusters = loop [] StringSet.empty highest_density_first in
+  L.append clusters singletons
 
 let main () =
   Log.(set_log_level INFO);
@@ -93,22 +99,19 @@ let main () =
   let threshold = CLI.get_float_def ["-t"] args 0.9 in
   assert(threshold >= 0.0 && threshold <= 1.0);
   verbose := CLI.get_set_bool ["-v"] args;
+  let nprocs = CLI.get_int_def ["-np"] args 1 in
   CLI.finalize ();
   let all_mols = FpMol.molecules_of_file input_fn in
   let total = L.length all_mols in
   Log.info "read %d from %s" total input_fn;
   Log.info "BST OK";
-  let clusters = butina threshold all_mols in
-  Log.info "clusters: %d" (Ht.length clusters);
-  let i = ref 0 in
+  let clusters = butina nprocs threshold all_mols in
+  Log.info "clusters: %d" (L.length clusters);
   Utls.with_out_file output_fn (fun out ->
-      Ht.iter (fun center members ->
+      L.iteri (fun cid (center, members, size) ->
           fprintf out "cid: %d size: %d center: %s members:"
-            !i (L.length members) (FpMol.get_name center);
-          incr i;
-          L.iter (fun mol ->
-              fprintf out " %s" (FpMol.get_name mol)
-            ) members;
+            cid size (FpMol.get_name center);
+          StringSet.iter (fprintf out " %s") members;
           fprintf out "\n"
         ) clusters
     )
