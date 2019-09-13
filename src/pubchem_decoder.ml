b3ec110e332549f14e3588c6e692c5f96c2fd3b5
Francois Berenger <unixjunkie@sdf.org>
2019-09-13 16:56:26 +0900

    Pubchem_decoder: also accept lECFP6 (16384 bits)
    instead of just PubChem-881 bits FP

--- src/pubchem_decoder.ml
+++ src/pubchem_decoder.ml
@@ -24,8 +24,8 @@ let liblinear_line_of_pubchem_line line =
   | [name; _IC50; bitstring] ->
     let is_active = String.starts_with name "active" in
     let nb_bits = String.length bitstring in
-    assert(nb_bits = 881);
-    let buff = Buffer.create 80 in
+    assert(nb_bits = 881 || nb_bits = 16384);
+    let buff = Buffer.create 16500 in
     Buffer.add_string buff (if is_active then "+1" else "-1");
     String.iteri (fun i c ->
         if c = '1' then
