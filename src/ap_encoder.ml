746e680b1f68ea345c7b6a61b9b34fe2ae85f09c
Francois Berenger <unixjunkie@sdf.org>
2020-04-28 14:33:27 +0900

    Ap_encoder: WIP

--- src/ap_encoder.ml
+++ src/ap_encoder.ml
@@ -10,8 +10,33 @@
 
 open Printf
 
-type mode = Read_dictionary of string
-          | Create_dictionary of string
+module CLI = Minicli.CLI
+module Ht = BatHashtbl
+module Log = Dolog.Log
+module Utls = Molenc.Utls
+
+type filename = string
+
+type mode = Input_dictionary of filename
+          | Output_dictionary of filename
+
+(* reconstruct the map feat->featId from given file *)
+let dico_from_file fn =
+  let n = Utls.count_lines_of_file fn in
+  assert(n > 1);
+  let feat2id = Ht.create (n - 1) in
+  let header = Utls.get_first_line fn in
+  Utls.enforce (header = "#atom_pairs")
+    ("Ap_encoder.dico_from_file: not an atom pairs dict: " ^ fn);
+  Utls.iter_on_lines_of_file fn (fun line ->
+      if not (BatString.starts_with line "#") then
+        Scanf.sscanf line "%d %s" (fun id feat ->
+            (* the binding defined in the dictionary should be unique *)
+            assert(not (Ht.mem feat2id feat));
+            Ht.add feat2id feat id
+          )
+    );
+  feat2id
 
 let main () =
   Log.(set_log_level INFO);
@@ -19,17 +44,27 @@ let main () =
   let argc, args = CLI.init () in
   if argc = 1 then
     (eprintf "usage:\n  \
-              %s -i molecules.types -o molecules.pairs\n  \
-              -i <filename>: where to read molecules from\n  \
-              -o <filename>: where to write encoded molecules\n  \
-              -od <filename>: create and write encoding dictionary\n  \
-                              (incompatible with -id)\n  \
-              -id <filename>: read existing feature dictionary from file\n  
-                              (incompatible with -od)\n"
+              %s -i molecules.types -o molecules.pairs {-od|-id} ap.dix\n  \
+              -i <filename>: input molecules file\n  \
+              -o <filename>: encoded molecules output file\n  \
+              -od <filename>: create and write feature dictionary\n      \
+              (incompatible with -id)\n  \
+              -id <filename>: read existing feature dictionary\n      \
+              (incompatible with -od)\n"
        Sys.argv.(0);
      exit 1);
   let input_fn = CLI.get_string ["-i"] args in
   let output_fn = CLI.get_string ["-o"] args in
+  let dico_mode = match (CLI.get_string_opt ["-id"] args,
+                         CLI.get_string_opt ["-od"] args) with
+  | (None, None) -> failwith "Ap_encoder: provide either -id or -od"
+  | (Some _, Some _) -> failwith "Ap_encoder: -id and -od are exclusive"
+  | (Some id_fn, None) -> Input_dictionary id_fn
+  | (None, Some od_fn) -> Output_dictionary od_fn in
+  CLI.finalize ();
+  let dico = match dico_mode with
+    | Input_dictionary id_fn -> dico_from_file id_fn
+    | Output_dictionary od_fn -> failwith "not implemented yet" in
   failwith "not implemented yet"
 
 let () = main ()
