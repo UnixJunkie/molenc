8e96b67e752033f428eb1838613df1f876510d99
Francois Berenger <unixjunkie@sdf.org>
2020-06-19 15:04:31 +0900

    tweak in bin/rgb_scale.py

--- bin/rgb_scale.py
+++ bin/rgb_scale.py
@@ -5,7 +5,7 @@
 from colour import Color
 
 red = Color("red")
-colors = list(red.range_to(Color("white"), 100))
+colors = list(red.range_to(Color("white"), 101))
 for c in colors:
     (r, g, b) = c.get_rgb()
     print("%.2f %.2f %.2f" % (r, g, b))
