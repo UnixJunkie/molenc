87be38b05efe620ebf67758bdebe5f8f39cfea71
Francois Berenger <unixjunkie@sdf.org>
2020-06-22 15:11:45 +0900

    bin/smi2svg.py: looks good enough

--- bin/smi2svg.py
+++ bin/smi2svg.py
@@ -8,6 +8,8 @@ import argparse
 import rdkit
 import sys
 from rdkit import Chem
+from rdkit.Chem import AllChem
+from rdkit.Chem.Draw import rdMolDraw2D
 
 def RobustMolSupplier(filename):
     with open(filename) as f:
@@ -27,7 +29,15 @@ if __name__ == '__main__':
         sys.stderr.write("usage: %s input.smi\n" % sys.argv[0])
         sys.exit(1)
     input_smi = sys.argv[1]
-    for i, name, mol in RobustMolSupplier(input_inchi):
+    for i, name, mol in RobustMolSupplier(input_smi):
         if mol is None:
             continue
-    output.close()
+        AllChem.Compute2DCoords(mol) # generate 2D conformer
+        d = rdMolDraw2D.MolDraw2DSVG(200, 200)
+        # d.drawOptions().addAtomIndices = True
+        caption = '%d %s' % (i, name)
+        d.DrawMolecule(mol, legend = caption)
+        d.FinishDrawing()
+        out_fn = '%d.svg' % i
+        with open(out_fn, 'w') as out:
+            out.write(d.GetDrawingText())
