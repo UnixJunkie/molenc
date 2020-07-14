992cb62f15c49f3f4f731152cac722ff21219575
Francois Berenger <unixjunkie@sdf.org>
2020-07-14 17:00:07 +0900

    Lean: looks like it is working

--- src/lean.ml
+++ src/lean.ml
@@ -13,13 +13,14 @@
 
 open Printf
 
-module A = Array
+module A = BatArray
 module CLI = Minicli.CLI
 module F = BatFloat
 module L = BatList
 module Log = Dolog.Log
 module Rand = BatRandom
 module Stats = Owl.Stats
+module Utls = Molenc.Utls
 
 let get_field_as_float s f line =
   try
@@ -32,13 +33,18 @@ let get_field_as_float s f line =
                  in line: %s" f s line in
     raise exn
 
-let log_KS_test n Owl.Stats.{reject; p_value; score} =
-  if reject then
-    Log.warn "N=%d dists differ.; KS: %.3f p-value: %.3f"
-      n p_value score
+let log_KS_test n alpha ks_res =
+  let null_rejected = Owl.Stats.(ks_res.reject) in
+  let p_value = Owl.Stats.(ks_res.p_value) in
+  let ks_stat = Owl.Stats.(ks_res.score) in
+  if p_value < alpha then
+    (* distributions differ *)
+    Log.warn "differ N=%d KS: %.3f p: %.3f (a=%.3f) rej=%d"
+      n ks_stat p_value alpha (Utls.int_of_bool null_rejected)
   else
-    Log.info "N=%d same dist.; KS: %.3f p-value: %.3f"
-      n p_value score
+    (* distributions are the same *)
+    Log.info "same N=%d KS: %.3f p: %.3f (a=%.3f) rej=%d"
+      n ks_stat p_value alpha (Utls.int_of_bool null_rejected)
 
 let main () =
   Log.(set_log_level INFO);
@@ -48,7 +54,9 @@ let main () =
     begin
       eprintf "usage:\n\
                %s\n  \
-               -b <int>: batch size (default=5%% of max)\n  \
+               -b0 <int>: first batch size\n  \
+               -b <int>: batch size (bootstrap size increment; \
+               default=5%% of max)\n  \
                -i <filename>: input file\n  \
                -s <char>: field separator (default=\\t)\n  \
                -f <int>: field number (starts from 1)\n  \
@@ -60,36 +68,33 @@ let main () =
   let sep = CLI.get_char_def ["-s"] args '\t' in
   let field = (CLI.get_int ["-f"] args) - 1 in
   let input_fn = CLI.get_string ["-i"] args in
-  let rand_lines =
-    let all_lines = Molenc.Utls.lines_of_file input_fn in
-    L.shuffle ~state:(Rand.State.make_self_init ()) all_lines in
-  let n = L.length rand_lines in
+  let all_lines = Utls.lines_of_file input_fn in
+  let n = L.length all_lines in
   let batch_size =
     CLI.get_int_def ["-b"] args
       (int_of_float (F.ceil (0.05 *. (float n)))) in
+  let batch_size0 = CLI.get_int_def ["-b0"] args batch_size in
   CLI.finalize ();
   Log.info "batch size: %d" batch_size;
-  let all_values =
-    A.of_list
-      (L.map (get_field_as_float sep field) rand_lines) in
-  A.sort BatFloat.compare all_values; (* sort in increasing order *)
-  let curr_sample = ref (L.take batch_size rand_lines) in
-  let next_sample = ref (L.take (2 * batch_size) rand_lines) in
-  let total = ref (L.length !curr_sample) in
+  let total = ref 0 in
   while !total <= n do
-    let smaller_sample = 
-      A.of_list (L.map (get_field_as_float sep field) !curr_sample) in
+    let smaller_sample =
+      A.of_list
+        (L.map (get_field_as_float sep field)
+           (L.take (if !total = 0 then batch_size0 else !total)
+              (L.shuffle ~state:(Rand.State.make_self_init ()) all_lines)))
+    in
+    total := (A.length smaller_sample) + batch_size;
     let bigger_sample =
-      A.of_list (L.map (get_field_as_float sep field) !next_sample) in
+      A.of_list
+        (L.map (get_field_as_float sep field)
+           (L.take !total
+              (L.shuffle ~state:(Rand.State.make_self_init ()) all_lines)))
+    in
     A.sort BatFloat.compare smaller_sample;
     A.sort BatFloat.compare bigger_sample;
-    let ks = Stats.ks2_test ~alpha:alpha smaller_sample all_values in
-    log_KS_test !total ks;
-    (* let ks = Stats.ks2_test ~alpha:alpha smaller_sample bigger_sample in
-     * log_KS_test !total ks; *)
-    total := !total + batch_size;
-    curr_sample := !next_sample;
-    next_sample := L.take !total rand_lines
+    let ks = Stats.ks2_test ~alpha smaller_sample bigger_sample in
+    log_KS_test !total alpha ks
   done
 
 let () = main ()
