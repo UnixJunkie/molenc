a3c1fb67c05da61685a63101f802c1f50b9d86ae
Francois Berenger <unixjunkie@sdf.org>
2020-07-15 14:57:25 +0900

    bin/molenc_lizard.py: diagnostic string in case an alien is encountered

--- bin/molenc_lizard.py
+++ bin/molenc_lizard.py
@@ -29,11 +29,11 @@ PeriodicTable = Chem.GetPeriodicTable()
 
 def RobustSmilesMolSupplier(filename):
     with open(filename) as f:
-        for line in f:
+        for i, line in enumerate(f):
             words = line.split()
             smile = words[0]
             name = words[1]
-            yield (Chem.MolFromSmiles(smile), name)
+            yield (i, Chem.MolFromSmiles(smile), name)
 
 # detect strange molecules
 def is_alien(MolW, cLogP, TPSA, RotB, HBA, HBD, FC):
@@ -44,12 +44,38 @@ def is_alien(MolW, cLogP, TPSA, RotB, HBA, HBD, FC):
     # Step 2: ignored; too complex (using IC50 curve shape + other things).
     # Step 3: Molecular property range filter. Remaining
     #         actives and inactives were kept if:
+    # FBR: !!! check rdkit's LogP Vs. pipeline pilot's AlogP !!!
+    # rdkit's MolW unit seems to be g/mol
+    # 1C -> 12Da = 12g/mol <=> 1Da = 1g/mol
     return (MolW <= 150 or MolW >= 800 or # 150 < MolW < 800 Da
             cLogP <= -3.0 or cLogP >= 5.0 or # −3.0 < AlogP < 5.0
             RotB >= 15 or # RotB < 15
             HBA >= 10 or # HBA < 10
             HBD >= 10 or # HBD < 10
-            FC <= -2 or FC > 2) # −2.0 < FC < 2.0
+            FC <= -2 or FC >= 2) # −2.0 < FC < 2.0
+
+# message string describing the alien
+def alien_diagnose(i, name, MolW, cLogP, TPSA, RotB, HBA, HBD, FC):
+    err_msg = "%d %s" % (i, name)
+    if MolW <= 150:
+        err_msg += "; (MolW=%g) <= 150" % MolW
+    if MolW >= 800:
+        err_msg += "; (MolW=%g) >= 800" % MolW
+    if cLogP <= -3.0:
+        err_msg += "; (cLogP=%g) <= -3" % cLogP
+    if cLogP >= 5.0:
+        err_msg += "; (cLogP=%g) >= 5" % cLogP
+    if RotB >= 15:
+        err_msg += "; (RotB=%d) >= 15" % RotB
+    if HBA >= 10:
+        err_msg += "; (HBA=%d) >= 10" % HBA
+    if HBD >= 10:
+        err_msg += "; (HBD=%d) >= 10" % HBD
+    if FC <= -2:
+        err_msg += "; (FC=%d) <= -2" % FC
+    if FC >= 2:
+        err_msg += "; (FC=%d) >= 2" % FC
+    return err_msg
 
 def main():
     # CLI options parsing
@@ -76,7 +102,7 @@ def main():
     error_count = 0
     with open(output_csv, 'w') as out_file:
         print("#name,MolW,cLogP,TPSA,RotB,HBA,HBD,FC", file=out_file)
-        for mol, name in RobustSmilesMolSupplier(input_smi):
+        for i, mol, name in RobustSmilesMolSupplier(input_smi):
             if mol is None:
                 error_count += 1
             else:
@@ -90,8 +116,9 @@ def main():
                 FC = Chem.rdmolops.GetFormalCharge(mol)
                 alien = is_alien(MolW, cLogP, TPSA, RotB, HBA, HBD, FC)
                 if alien:
-                    # FBR: show alien characteristics to the user
-                    print("WARN: alien: %s" % name, file=sys.stderr)
+                    alien_str = alien_diagnose(i, name, MolW, cLogP, TPSA,
+                                               RotB, HBA, HBD, FC)
+                    print("WARN: %s" % alien_str, file=sys.stderr)
                     alien_count += 1
                 if (not alien) or (not rm_aliens):
                     csv_line = "%s,%g,%g,%g,%d,%d,%d,%d" % \
