c6bcfc2af7ddf3033d2da8f57b2b14f6c6a23959
Francois Berenger <unixjunkie@sdf.org>
2020-12-14 15:19:50 +0900

    Fragmentable_mol: need to test

--- src/fragmentable_mol.ml
+++ src/fragmentable_mol.ml
@@ -27,6 +27,13 @@ let atom_of_string s =
         formal_charge = l }
     )
 
+let string_of_atom a =
+  sprintf "%d,%d,%d,%d"
+    a.pi_electrons
+    a.atomic_num
+    a.heavy_neighbors
+    a.formal_charge
+
 type bond_type = Single
                | Aromatic
                | Double
@@ -40,6 +47,12 @@ let bond_of_char = function
   | c -> failwith ("Fragmentable_mol.bond_of_char: unsupported: " ^
                    (String.make 1 c))
 
+let char_of_bond = function
+  | Single -> '-'
+  | Aromatic -> '~'
+  | Double -> '='
+  | Triple -> '#'
+
 type bond = { start: int;
               btype: bond_type;
               stop: int }
@@ -51,6 +64,9 @@ let bond_of_string s =
         stop }
     )
 
+let string_of_bond b =
+  sprintf "%d %c %d" b.start (char_of_bond b.btype) b.stop
+
 (* a molecule with fragmentation hints *)
 type fragmentable =
   { atoms: atom array;
@@ -58,16 +74,27 @@ type fragmentable =
     cut_bonds: int array; (* indexes in the bonds array *)
     frag_hint: int } (* how many bonds are suggested to be broken *)
 
-type attachment_point = { start: int; (* atom index *)
-                          dest: atom } (* end-point allowed atom type *)
+type attach = { start: int; (* atom index *)
+                dest: atom } (* end-point allowed atom type *)
 
-let dummy_attachment_point = { start = -1;
-                               dest = dummy_atom }
+let dummy_attach = { start = -1;
+                     dest = dummy_atom }
+
+let string_of_attach a =
+  sprintf "%d %s" a.start (string_of_atom a.dest)
 
 type fragmented =
   { atoms: atom array;
     bonds: bond array;
-    anchors: attachment_point array }
+    anchors: attach array }
+
+let write_one_fragment out frag =
+  fprintf out "atoms:%d\n" (A.length frag.atoms);
+  A.iter (fun a -> fprintf out "%s\n" (string_of_atom a)) frag.atoms;
+  fprintf out "bonds:%d\n" (A.length frag.bonds);
+  A.iter (fun b -> fprintf out "%s\n" (string_of_bond b)) frag.bonds;
+  fprintf out "anchors:%d\n" (A.length frag.anchors);
+  A.iter (fun a -> fprintf out "%s\n" (string_of_attach a)) frag.anchors
 
 let parse_mol_header header =
   (* "^#atoms:19 NCGC00261763-01$" *)
@@ -117,10 +144,9 @@ let read_one_molecule (input: in_channel): fragmentable =
     frag_hint }
 
 (* translate bonds to cut into attachment points *)
-let attach_points (mol: fragmentable) (cut_bonds: int array)
-  : attachment_point array =
+let attach_points (mol: fragmentable) (cut_bonds: int array): attach array =
   let n = A.length cut_bonds in
-  let res = A.make (2*n) dummy_attachment_point in
+  let res = A.make (2*n) dummy_attach in
   A.iteri (fun i to_cut ->
       let j = 2*i in
       let k = j + 1 in
@@ -186,9 +212,27 @@ let connected_component (seed: int) (succs: succs_ht): IS.t =
   in
   loop (IS.singleton seed) IS.empty IS.empty
 
+let enforce_conn_comp (comp: IS.t) (frag: fragmented): fragmented =
+  { atoms = A.filteri (fun i _x -> IS.mem i comp) frag.atoms;
+    bonds =
+      A.filter (fun (b: bond) ->
+          (IS.mem b.start comp) && (IS.mem b.stop comp)
+        ) frag.bonds;
+    anchors = A.filter (fun a -> IS.mem a.start comp) frag.anchors }
+
 (* cut a fragmented molecule into its disconnected fragments *)
-let reconcile (_frag: fragmented): fragmented list =
-  failwith "not implemented yet"
+let reconcile (frag: fragmented): fragmented list =
+  let succs = compute_successors frag in
+  let processed = ref IS.empty in
+  A.fold_left (fun acc (a: attach) ->
+      if IS.mem a.start !processed then
+        acc
+      else
+        let conn = connected_component a.start succs in
+        let res = enforce_conn_comp conn frag in
+        processed := IS.union conn !processed;
+        res :: acc
+    ) [] frag.anchors
 
 (* we fragment the molecule just once;
    larger molecules give rise to more fragments *)
@@ -198,12 +242,9 @@ let fragment_molecule rng m =
   assert(A.length cuttable >= nb_cuts);
   A.shuffle ~state:rng cuttable;
   let to_cut = A.left cuttable nb_cuts in
-  let _edited = edit_bonds m to_cut in
+  let edited = edit_bonds m to_cut in
   (* this freshly cut molecule needs to be "reconciliated" *)
-  (* compute connected components *)
-  (* renumber nodes *)
-  (* translate edges *)
-  failwith "not implemented yet"
+  reconcile edited
 
 let main () =
   (* read in a file *)
@@ -212,19 +253,29 @@ let main () =
   let argc, args = CLI.init () in
   if argc = 1 then
     (eprintf "usage:\n  \
-              %s -i molecules.txt\n"
+              %s -i molecules.txt -o out.txt [-s rng_seed:int]\n"
        Sys.argv.(0);
      exit 1);
   let input_fn = CLI.get_string ["-i"] args in
+  let output_fn = CLI.get_string ["-o"] args in
+  let rng = match CLI.get_int_opt ["-s"] args with
+    | Some s -> BatRandom.State.make [|s|] (* repeatable *)
+    | None -> BatRandom.State.make_self_init () in
   CLI.finalize();
-  let _all_molecules =
+  let all_molecules =
     Utls.with_in_file input_fn (fun input ->
         let res, exn =
           L.unfold_exn (fun () -> read_one_molecule input) in
         (if exn <> End_of_file then raise exn);
         res
       ) in
-  (* TODO: really fragment it then *)
-  ()
+  (* fragment them *)
+  (* FBR: parallelizable, if needed *)
+  Utls.with_out_file output_fn (fun out ->
+      L.iter (fun mol ->
+          let frags = fragment_molecule rng mol in
+          L.iter (write_one_fragment out) frags
+        ) all_molecules
+    )
 
 let () = main ()
