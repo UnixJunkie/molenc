2d90e23f550cc6380a0eb15538780add46a758c4
Francois Berenger <unixjunkie@sdf.org>
2020-12-15 18:19:25 +0900

    mol_frag_test.sh: also test molgen

--- mol_frag_test.sh
+++ mol_frag_test.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-rm -f data/3.to_frag
+rm -f data/3.to_frag data/3.frags data/3.mols
 ./bin/molenc_frag.py -i data/3.smi -o data/3.to_frag
 ./molenc_frag -im data/3.to_frag -of data/3.frags -s 1234
+./molenc_frag -if data/3.frags -om data/3.mols -s 1234 -n 3
