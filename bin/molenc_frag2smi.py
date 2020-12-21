496f77bcbf6c622dbc4d75d69b7e62699d0aa0d0
Francois Berenger <unixjunkie@sdf.org>
2020-12-21 16:05:23 +0900

    WIP in bin/molenc_frag2smi.py

--- bin/molenc_frag2smi.py
+++ bin/molenc_frag2smi.py
@@ -9,7 +9,7 @@
 
 # atom typing and molecule fragmentation hints
 
-import argparse, molenc_common, os, random, rdkit, sys, time
+import argparse, molenc_common, os, random, rdkit, re, sys, time
 
 from enum import Enum
 from rdkit import Chem
@@ -18,80 +18,69 @@ from rdkit.Chem import AllChem, Descriptors
 from rdkit.Chem.AtomPairs import Pairs
 from rdkit.Chem.Draw import rdMolDraw2D
 
-def RobustSmilesMolSupplier(filename):
-    with open(filename) as f:
-        for line in f:
-            words = line.split()
-            smile = words[0]
-            name = " ".join(words[1:]) # everything after the SMILES string
-            yield (name, Chem.MolFromSmiles(smile))
+# def type_atom(a):
+#     # stereo chemistry is ignored for the moment
+#     nb_pi_electrons = Pairs.Utils.NumPiElectrons(a)
+#     atom_num = a.GetAtomicNum()
+#     nbHA = nb_heavy_atom_neighbors(a)
+#     formal_charge = a.GetFormalCharge()
+#     # make this easy to parse / unambiguous
+#     res = "%d,%d,%d,%d" % (nb_pi_electrons, atom_num, nbHA, formal_charge)
+#     return res
 
-def nb_heavy_atom_neighbors(a):
-    res = 0
-    for neighb in a.GetNeighbors():
-        if neighb.GetAtomicNum() != 1:
-            res += 1
-    return res
+# def char_of_bond_type(bond):
+#     t = bond.GetBondType()
+#     if t == rdkit.Chem.rdchem.BondType.SINGLE:
+#         return '-'
+#     elif t == rdkit.Chem.rdchem.BondType.AROMATIC:
+#         return '~'
+#     elif t == rdkit.Chem.rdchem.BondType.DOUBLE:
+#         return '='
+#     elif t == rdkit.Chem.rdchem.BondType.TRIPLE:
+#         return '#'
+#     else:
+#         assert("molenc_frag.py: char_of_bond_type" == "")
 
-def type_atom(a):
-    # stereo chemistry is ignored for the moment
-    nb_pi_electrons = Pairs.Utils.NumPiElectrons(a)
-    atom_num = a.GetAtomicNum()
-    nbHA = nb_heavy_atom_neighbors(a)
-    formal_charge = a.GetFormalCharge()
-    # make this easy to parse / unambiguous
-    res = "%d,%d,%d,%d" % (nb_pi_electrons, atom_num, nbHA, formal_charge)
-    return res
+# "#atoms:15 NCGC00261552-01_f00"
+def read_atoms_header_line(line):
+    (atoms, nb_atoms, frag_name) = [t(s) for t,s in
+                                    zip((str,int,str), re.split('[: ]', line))]
+    assert(atoms == "#atoms")
+    return (nb_atoms, frag_name)
 
-# single bonds not in rings
-def find_cuttable_bonds(mol):
-    res = []
-    for b in mol.GetBonds():
-        if ((b.GetBondType() == rdkit.Chem.rdchem.BondType.SINGLE) and
-            (not b.IsInRing())):
-            res.append(b)
-    return res
+# "0 0,6,2,0"
+def read_atom_line(line):
+    (index, nb_pi, atomic_num, nb_HA, charge) = [t(s) for t,s in
+                                                 zip((int,int,int,int,int),
+                                                     re.split('[, ]', line))]
+    return (index, nb_pi, atomic_num, nb_HA, charge)
 
-def print_typed_atoms(out, mol):
-    for a in mol.GetAtoms():
-        i = a.GetIdx()
-        t = type_atom(a)
-        print("%d %s" % (i, t), file=out)
+# "#bonds:16"
+def read_bonds_header_line(line):
+    (bonds, nb_bonds) = [t(s) for t,s in
+                         zip((str,int), re.split('[:]', line))]
+    assert(bonds == "#bonds")
+    return nb_bonds
 
-def char_of_bond_type(bond):
-    t = bond.GetBondType()
-    if t == rdkit.Chem.rdchem.BondType.SINGLE:
-        return '-'
-    elif t == rdkit.Chem.rdchem.BondType.AROMATIC:
-        return '~'
-    elif t == rdkit.Chem.rdchem.BondType.DOUBLE:
-        return '='
-    elif t == rdkit.Chem.rdchem.BondType.TRIPLE:
-        return '#'
-    else:
-        assert("molenc_frag.py: char_of_bond_type" == "")
+# "0 - 16"
+def read_bond_line(line):
+    (start_i, bond_order_char, stop_i) = [t(s) for t,s in
+                                          zip((int,str,int),
+                                              re.split('[ ]', line))]
+    return (start_i, bond_order_char, stop_i)
 
-# print all bonds with their type
-def print_bonds(out, mol):
-    print("#bonds:%d" % mol.GetNumBonds(), file=out)
-    bonds = mol.GetBonds()
-    for bond in bonds:
-        a = bond.GetBeginAtomIdx()
-        b = bond.GetEndAtomIdx()
-        t = char_of_bond_type(bond)
-        print("%d %c %d" % (a, t, b), file=out)
+# "#anchors:1"
+def read_anchors_header_line(line):
+    (anchors, nb_anchors) = [t(s) for t,s in
+                             zip((int), re.split('[:]', line))]
+    assert(anchors == "#anchors")
+    return nb_anchors
 
-# print which bonds are cuttable and the suggested number of cuts
-def print_cuttable_bonds(out, mol):
-    cuttable_bonds = find_cuttable_bonds(mol)
-    total_weight = Descriptors.MolWt(mol)
-    # 150 Da: suggested max fragment weight
-    nb_frags = round(total_weight / 150)
-    max_cuts = min(len(cuttable_bonds), nb_frags)
-    print("#cut_bonds:%d:%d" % (len(cuttable_bonds), max_cuts), file=out)
-    for bond in cuttable_bonds:
-        i = bond.GetIdx()
-        print("%d" % i, file=out)
+# "0,6,2,0 0 0,6,2,0"
+def read_anchor_line(line):
+    (start_t, start_i, stop_t) = [t(s) for t,s in
+                                  zip((str,int,str), re.split('[ ]', line))]
+    return start_i
 
 if __name__ == '__main__':
     before = time.time()
