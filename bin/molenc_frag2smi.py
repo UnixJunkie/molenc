64ec9cb8f9580c6ecb7916967db9b0cf3a6da49b
Francois Berenger <unixjunkie@sdf.org>
2020-12-21 16:37:11 +0900

    bin/molenc_frag2smi.py: WIP
    after many lines of Python plus many tests, we start to get
    something working...

--- bin/molenc_frag2smi.py
+++ bin/molenc_frag2smi.py
@@ -7,80 +7,94 @@
 # Kyushu Institute of Technology,
 # 680-4 Kawazu, Iizuka, Fukuoka, 820-8502, Japan.
 
-# atom typing and molecule fragmentation hints
+# molecule txt fragment to SMILES
 
-import argparse, molenc_common, os, random, rdkit, re, sys, time
-
-from enum import Enum
+import argparse, rdkit, re, sys, time
 from rdkit import Chem
-from rdkit import RDConfig
-from rdkit.Chem import AllChem, Descriptors
-from rdkit.Chem.AtomPairs import Pairs
-from rdkit.Chem.Draw import rdMolDraw2D
-
-# def type_atom(a):
-#     # stereo chemistry is ignored for the moment
-#     nb_pi_electrons = Pairs.Utils.NumPiElectrons(a)
-#     atom_num = a.GetAtomicNum()
-#     nbHA = nb_heavy_atom_neighbors(a)
-#     formal_charge = a.GetFormalCharge()
-#     # make this easy to parse / unambiguous
-#     res = "%d,%d,%d,%d" % (nb_pi_electrons, atom_num, nbHA, formal_charge)
-#     return res
-
-# def char_of_bond_type(bond):
-#     t = bond.GetBondType()
-#     if t == rdkit.Chem.rdchem.BondType.SINGLE:
-#         return '-'
-#     elif t == rdkit.Chem.rdchem.BondType.AROMATIC:
-#         return '~'
-#     elif t == rdkit.Chem.rdchem.BondType.DOUBLE:
-#         return '='
-#     elif t == rdkit.Chem.rdchem.BondType.TRIPLE:
-#         return '#'
-#     else:
-#         assert("molenc_frag.py: char_of_bond_type" == "")
 
 # "#atoms:15 NCGC00261552-01_f00"
-def read_atoms_header_line(line):
+def read_atoms_header(line):
     (atoms, nb_atoms, frag_name) = [t(s) for t,s in
                                     zip((str,int,str), re.split('[: ]', line))]
     assert(atoms == "#atoms")
     return (nb_atoms, frag_name)
 
 # "0 0,6,2,0"
-def read_atom_line(line):
+def read_atom(line):
     (index, nb_pi, atomic_num, nb_HA, charge) = [t(s) for t,s in
                                                  zip((int,int,int,int,int),
                                                      re.split('[, ]', line))]
     return (index, nb_pi, atomic_num, nb_HA, charge)
 
 # "#bonds:16"
-def read_bonds_header_line(line):
+def read_bonds_header(line):
     (bonds, nb_bonds) = [t(s) for t,s in
                          zip((str,int), re.split('[:]', line))]
     assert(bonds == "#bonds")
     return nb_bonds
 
+def bond_type_of_char(c):
+    if c == '-':
+        return rdkit.Chem.rdchem.BondType.SINGLE
+    elif c == '~':
+        return rdkit.Chem.rdchem.BondType.AROMATIC
+    elif c == '=':
+        return rdkit.Chem.rdchem.BondType.DOUBLE
+    elif c == '#':
+        return rdkit.Chem.rdchem.BondType.TRIPLE
+    else:
+        assert("molenc_frag2smi.py: bond_type_of_char" == "")
+
 # "0 - 16"
-def read_bond_line(line):
-    (start_i, bond_order_char, stop_i) = [t(s) for t,s in
+def read_bond(line):
+    (start_i, c, stop_i) = [t(s) for t,s in
                                           zip((int,str,int),
                                               re.split('[ ]', line))]
-    return (start_i, bond_order_char, stop_i)
+    return (start_i, bond_type_of_char(c), stop_i)
 
 # "#anchors:1"
-def read_anchors_header_line(line):
+def read_anchors_header(line):
     (anchors, nb_anchors) = [t(s) for t,s in
-                             zip((int), re.split('[:]', line))]
+                             zip((str,int), re.split('[:]', line))]
     assert(anchors == "#anchors")
     return nb_anchors
 
 # "0,6,2,0 0 0,6,2,0"
-def read_anchor_line(line):
+def read_anchor(line):
     (start_t, start_i, stop_t) = [t(s) for t,s in
                                   zip((str,int,str), re.split('[ ]', line))]
     return start_i
+
+class End_of_file(Exception):
+    """End of file was reached"""
+    pass
+
+def read_one_fragment(input):
+    atoms_header = input.readline()
+    if atoms_header == '':
+        raise End_of_file
+    nb_atoms, frag_name = read_atoms_header(atoms_header)
+    atoms = []
+    for i in range(nb_atoms):
+        line = input.readline()
+        atom = read_atom(line)
+        atoms.append(atom)
+    bonds_header = input.readline()
+    nb_bonds = read_bonds_header(bonds_header)
+    bonds = []
+    for i in range(nb_bonds):
+        line = input.readline()
+        bond = read_bond(line)
+        bonds.append(bond)
+    anchors_header = input.readline()
+    nb_anchors = read_anchors_header(anchors_header)
+    anchors = []
+    for i in range(nb_anchors):
+        line = input.readline()
+        anchor = read_anchor(line)
+        anchors.append(anchor)
+    print('%s %d %d %d' % (frag_name, nb_atoms, nb_bonds, nb_anchors),
+          file=sys.stderr)
 
 if __name__ == '__main__':
     before = time.time()
@@ -97,25 +111,16 @@ if __name__ == '__main__':
         sys.exit(1)
     args = parser.parse_args()
     input_fn = args.input_fn
-    draw_mol = args.draw_mol
     output = open(args.output_fn, 'w')
-    mol_supplier = RobustSmilesMolSupplier(input_fn)
     count = 0
-    for name, mol in mol_supplier:
-        print("#atoms:%d %s" % (mol.GetNumAtoms(), name), file=output)
-        print_typed_atoms(output, mol)
-        print_bonds(output, mol)
-        print_cuttable_bonds(output, mol)
-        count += 1
-        if draw_mol:
-            d = rdMolDraw2D.MolDraw2DCairo(500, 500)
-            d.drawOptions().addAtomIndices = True
-            d.DrawMolecule(mol)
-            d.FinishDrawing()
-            png_fn = '%s.png' % name
-            with open(png_fn, 'wb') as fn:
-                fn.write(d.GetDrawingText())
+    with open(input_fn) as input:
+        try:
+            while True:
+                read_one_fragment(input)
+                count += 1
+        except End_of_file:
+            pass
     after = time.time()
     dt = after - before
-    print("%d molecules at %.2f mol/s" % (count, count / dt), file=sys.stderr)
+    print("%d fragments at %.2f frag/s" % (count, count / dt), file=sys.stderr)
     output.close()
