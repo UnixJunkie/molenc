26bd20238c84a5ca4be633e56197e771fc9b9f01
Francois Berenger <unixjunkie@sdf.org>
2020-12-22 12:16:50 +0900

    simpler regression tests

--- mol_frag_test.sh
+++ mol_frag_test.sh
@@ -5,7 +5,6 @@ set -x
 rm -f data/3.to_frag data/3.frags data/3.frags2 data/3.mols
 ./bin/molenc_frag.py -i data/3.smi -o data/3.to_frag
 ./molenc_frag -im data/3.to_frag -of data/3.frags -s 1234
-./molenc_frag -im data/3.to_frag -of data/3.frags2 -s 1234 -p 2
 ./molenc_frag -if data/3.frags -om data/3.mols -s 1234 -n 3
 
 # IN=data/all_kegg_drugs_20112019_std
