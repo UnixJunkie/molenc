b92dbcbcdd9662d6f77fe82c3c6ced73f93cc5dd
Francois Berenger <unixjunkie@sdf.org>
2020-12-23 17:45:52 +0900

    mol_frag_test.sh: more complete

--- mol_frag_test.sh
+++ mol_frag_test.sh
@@ -2,13 +2,21 @@
 
 set -x
 
-rm -f data/3.to_frag data/3.frags data/3.frags2 data/3.mols
+# clean
+rm -f data/3.to_frag data/3_frags.txt data/3_frags.smi data/3_genmols.txt data/3_genmols.smi
+
+# regen
 ./bin/molenc_frag.py -i data/3.smi -o data/3.to_frag
-./molenc_frag -im data/3.to_frag -of data/3.frags -s 1234
-./molenc_frag -if data/3.frags -om data/3.mols -s 1234 -n 3
+which mview && mview data/3.smi &
+./molenc_frag -im data/3.to_frag -of data/3_frags.txt -s 1234
+./bin/molenc_frag2smi.py -i data/3_frags.txt -o data/3_frags.smi
+which mview && mview data/3_frags.smi &
+./molenc_frag -if data/3_frags.txt -om data/3_genmols.txt -s 1234 -n 20
+./bin/molenc_mol2smi.py -i data/3_genmols.txt -o data/3_genmols.smi
+cut -f1 data/3_genmols.smi | sort -u > data/3_genmols_uniq.smi
+which mview && mview data/3_genmols_uniq.smi &
 
 # IN=data/all_kegg_drugs_20112019_std
 # ./bin/molenc_frag.py -i $IN.smi -o $IN.to_frag
-# ./bin/molenc_frag.py -i $IN.smi -o $IN.to_frag2 -p 2
 # ./molenc_frag -im $IN.to_frag -of $IN.frags -s 1234
 # ./molenc_frag -if $IN.frags -om $IN.mols -s 1234 -n 50
