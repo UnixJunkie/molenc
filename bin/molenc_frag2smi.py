b74f5a726cdb3b17a49b39aed8f6f93d0356f216
Francois Berenger <unixjunkie@sdf.org>
2021-01-12 16:59:37 +0900

    bin/molenc_frag2smi.py: WIP
    kekulization errors catch and correct

--- bin/molenc_frag2smi.py
+++ bin/molenc_frag2smi.py
@@ -12,6 +12,7 @@
 import argparse, rdkit, re, sys, time
 import molenc_common as common
 from rdkit import Chem
+from rdkit.Chem import AllChem
 from rdkit.Chem.Draw import rdMolDraw2D
 
 # "#anchors:1"
@@ -27,6 +28,35 @@ def read_anchor(line):
                                   zip((str,int,str), re.split('[ ]', line))]
     return start_i
 
+def current_valence(a):
+    res = 0.0
+    for b in a.GetBonds():
+        bt = b.GetBondType()
+        if bt == rdkit.Chem.rdchem.BondType.SINGLE:
+            res += 1.0
+        elif bt == rdkit.Chem.rdchem.BondType.AROMATIC:
+            res += 1.5
+        elif bt == rdkit.Chem.rdchem.BondType.DOUBLE:
+            res += 2.0
+        elif bt == rdkit.Chem.rdchem.BondType.TRIPLE:
+            res += 3.0
+    return res
+
+# try to correct a molecule that failed kekulization
+def restore_from_kekulization_error(mol):
+    for a in mol.GetAtoms():
+        anum = a.GetAtomicNum()
+        if a.IsInRing() and anum == 7:
+            val = current_valence(a)
+            if val == 4.0:
+                # set the correct +1 partial charge for Nitrogen with valence 4 in rings
+                a.SetFormalCharge(1)
+                a.SetNoImplicit(True) # forbid addition of implicit hydrogens
+                a.SetNumExplicitHs(0)
+            if val == 3.0:
+                a.SetNumExplicitHs(1) # must have one H then
+    return mol
+
 # create a fake molecule for the corresp. fragment
 def read_one_fragment(input):
     res_mol = Chem.RWMol()
@@ -35,7 +65,6 @@ def read_one_fragment(input):
         raise common.End_of_file # no EOF in Python...
     nb_atoms, frag_name = common.read_atoms_header(atoms_header)
     old2new = {}
-    pi_electrons = {}
     for _i in range(nb_atoms):
         line = input.readline().strip()
         (index, nb_pi, atomic_num, nb_HA, charge, stereo) = common.read_atom(line)
@@ -45,13 +74,8 @@ def read_one_fragment(input):
         if stereo > 0: # set chirality
             a.SetChiralTag(common.atom_stereo_code_to_chiral_tag(stereo))
         j = res_mol.AddAtom(a)
-        # # WARNING: we need a fix for aromatic Nitrogen ???
-        # if atomic_num == 7 and nb_pi == 1 and nb_HA == 2 and charge == 0:
-        #     a.SetNumExplicitHs(0)
         # we will need to convert atom indexes later
         old2new[index] = j
-        # need to remember pi electrons count
-        pi_electrons[j] = nb_pi
     bonds_header = input.readline().strip()
     nb_bonds = common.read_bonds_header(bonds_header)
     for i in range(nb_bonds):
@@ -65,13 +89,6 @@ def read_one_fragment(input):
     anchors_header = input.readline().strip()
     nb_anchors = read_anchors_header(anchors_header)
     anchors = []
-    # position aromaticity flag
-    for a in res_mol.GetAtoms():
-        anum = a.GetAtomicNum()
-        if a.IsInRing() and (anum == 6 or anum == 7):
-            j = a.GetIdx()
-            if pi_electrons[j] == 1:
-                a.SetIsAromatic(True)
     for _i in range(nb_anchors):
         line = input.readline().strip()
         anchor = read_anchor(line)
@@ -82,29 +99,40 @@ def read_one_fragment(input):
         # only single, non stereo, bonds out of rings have been cut
         res_mol.AddBond(start, j, Chem.rdchem.BondType.SINGLE)
         anchors.append(anchor)
+    # FBR: move this later on
+    # TRY TO PREVENT KEKULIZATION ERRORS LATER ON
+    pat1 = Chem.MolFromSmarts('c(=O)')
+    rep1 = Chem.MolFromSmarts('c(O)')
+    res_mol1 = AllChem.ReplaceSubstructs(res_mol, pat1, rep1)
+    res_mol = res_mol1[0]
     ## debug log
     # print('%s %d %d %d' % (frag_name, nb_atoms, nb_bonds, nb_anchors),
     #       file=sys.stderr)
     # smi for mol
-    error = False
+    kekul_error = False
     try:
         Chem.SanitizeMol(res_mol)
-    except rdkit.Chem.rdchem.AtomKekulizeException:
-        print("AtomKekulizeException in %s" % frag_name, file=sys.stderr)
-        error = True
     except rdkit.Chem.rdchem.KekulizeException:
         print("KekulizeException in %s" % frag_name, file=sys.stderr)
-        error = True
-    # if error:
-    #     d = rdMolDraw2D.MolDraw2DCairo(500, 500)
-    #     d.drawOptions().addAtomIndices = True
-    #     d.DrawMolecule(res_mol)
-    #     d.FinishDrawing()
-    #     png_fn = '%s.png' % frag_name
-    #     with open(png_fn, 'wb') as fn:
-    #         fn.write(d.GetDrawingText())
+        kekul_error = True
+    if kekul_error:
+        kekul_error2 = False
+        res_mol2 = restore_from_kekulization_error(res_mol)
+        try:
+            Chem.SanitizeMol(res_mol2)
+            print('restored one')
+        except rdkit.Chem.rdchem.KekulizeException:
+            print('failed restore\n%s\t%s' % (Chem.MolToSmiles(res_mol2), frag_name))
+            kekul_error2 = True
+        #   d = rdMolDraw2D.MolDraw2DCairo(500, 500)
+        #   d.drawOptions().addAtomIndices = True
+        #   d.DrawMolecule(res_mol)
+        #   d.FinishDrawing()
+        #   png_fn = '%s.png' % frag_name
+        #   with open(png_fn, 'wb') as fn:
+        #     fn.write(d.GetDrawingText())
     smi = Chem.MolToSmiles(res_mol)
-    return (smi, frag_name)
+    return (kekul_error, smi, frag_name)
 
 if __name__ == '__main__':
     before = time.time()
@@ -122,16 +150,24 @@ if __name__ == '__main__':
     args = parser.parse_args()
     input_fn = args.input_fn
     output = open(args.output_fn, 'w')
+    err_out = open(args.output_fn + '.err.smi', 'w')
     count = 0
+    kekul_err_count = 0
     with open(input_fn) as input:
         try:
             while True:
-                smi, name = read_one_fragment(input)
+                kekul_err, smi, name = read_one_fragment(input)
                 count += 1
-                print('%s\t%s' % (smi, name), file=output)
+                if kekul_err:
+                    print('%s\t%s' % (smi, name), file=err_out)
+                    kekul_err_count += 1
+                else:
+                    print('%s\t%s' % (smi, name), file=output)
         except common.End_of_file:
             pass
     after = time.time()
     dt = after - before
-    print("%d fragments at %.2f frag/s" % (count, count / dt), file=sys.stderr)
+    print("%d (%d kekul. errors) fragments at %.2f frag/s" %
+          (count, kekul_err_count, count / dt), file=sys.stderr)
     output.close()
+    err_out.close()
