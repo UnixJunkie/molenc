77a4cb99ce94b36093d0ba7b7266d747ce2c32ec
Francois Berenger <unixjunkie@sdf.org>
2021-01-14 14:52:44 +0900

    mol_frag_test.sh: quiet which

--- mol_frag_test.sh
+++ mol_frag_test.sh
@@ -6,14 +6,14 @@ set -x
 rm -f data/3.to_frag data/3_frags.txt data/3_frags.smi data/3_genmols.txt data/3_genmols.smi
 # regen
 ./bin/molenc_frag.py -i data/3.smi -o data/3.to_frag
-which mview && mview data/3.smi &
+(which mview > /dev/null) && mview data/3.smi &
 ./molenc_frag -im data/3.to_frag -of data/3_frags.txt -s 1234
 ./bin/molenc_frag2smi.py -i data/3_frags.txt -o data/3_frags.smi
-which mview && mview data/3_frags.smi &
+(which mview > /dev/null) && mview data/3_frags.smi &
 ./molenc_frag -if data/3_frags.txt -om data/3_genmols.txt -s 1234 -n 20
 ./bin/molenc_mol2smi.py -i data/3_genmols.txt -o data/3_genmols.smi
 cut -f1 data/3_genmols.smi | sort -u > data/3_genmols_uniq.smi
-which mview && mview data/3_genmols_uniq.smi &
+(which mview > /dev/null) && mview data/3_genmols_uniq.smi &
 
 # IN=data/chembl_antivirals
 # ./bin/molenc_frag.py -i $IN.smi -o $IN.to_frag --draw
