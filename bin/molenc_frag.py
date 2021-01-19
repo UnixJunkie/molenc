1f934f6f873c292fa580392b0a911b797e318315
Francois Berenger <unixjunkie@sdf.org>
2021-01-19 11:22:14 +0900

    bin/molenc_frag.py: some code was moved to molenc_common.py
    the new SMILES-based fragmenting scheme was moved to bin/molenc_smisur.py

--- bin/molenc_frag.py
+++ bin/molenc_frag.py
@@ -9,23 +9,17 @@
 
 # atom typing and molecule fragmentation hints
 
-import argparse, molenc_common, os, random, rdkit, sys, time
+import argparse
+import molenc_common as common
+import rdkit
+import sys
+import time
 
-from molenc_common import RobustSmilesMolSupplier
-from enum import IntEnum
+from common import RobustSmilesMolSupplier
 from rdkit import Chem
-from rdkit import RDConfig
-from rdkit.Chem import AllChem, Descriptors
-from rdkit.Chem.AtomPairs import Pairs
+from rdkit.Chem import Descriptors
 from rdkit.Chem.Draw import rdMolDraw2D
-from molenc_common import StereoCodes
-
-def nb_heavy_atom_neighbors(a):
-    res = 0
-    for neighb in a.GetNeighbors():
-        if neighb.GetAtomicNum() != 1:
-            res += 1
-    return res
+from common import StereoCodes
 
 to_stereo_code = \
   { # atom.SetChiralTag(Chem.ChiralType.CHI_UNSPECIFIED)
@@ -58,14 +52,6 @@ def get_atom_stereo_codes(m):
         res[i] = to_stereo_code[k]
     return res
 
-def get_stereo_center_indexes(m):
-    res = {}
-    # unassigned stereo centers are not reported;
-    # set includeUnassigned=True to change
-    for i, k in Chem.FindMolChiralCenters(m):
-        res[i] = True
-    return res
-
 # # stereo code tests
 # cis = Chem.MolFromSmiles('C/C=C\C')
 # trans = Chem.MolFromSmiles('C/C=C/C')
@@ -76,69 +62,11 @@ def get_stereo_center_indexes(m):
 # print(get_stereo_codes(l_ala))
 # print(get_stereo_codes(d_ala))
 
-def type_atom(a):
-    # stereo chemistry is ignored for the moment
-    nb_pi_electrons = Pairs.Utils.NumPiElectrons(a)
-    atom_num = a.GetAtomicNum()
-    nbHA = nb_heavy_atom_neighbors(a)
-    formal_charge = a.GetFormalCharge()
-    # make this easy to parse / unambiguous
-    res = "%d,%d,%d,%d" % (nb_pi_electrons, atom_num, nbHA, formal_charge)
-    return res
-
-def type_atoms(mol):
-    return [type_atom(a) for a in mol.GetAtoms()]
-
-def log_protected_bond(name, b):
-    print('mol %s: protected bond %d' % (name, b.GetIdx()))
-
-# only single bonds not in rings, no stereo bonds,
-# no bond to/from a specified stereo center (i.e. if stereo was set,
-# we protect it)
-def find_cuttable_bonds(mol):
-    name = mol.GetProp("name")
-    stereo_center_indexes = get_stereo_center_indexes(mol)
-    for b in mol.GetBonds():
-        # protect bonds to/from a stereo center
-        i = b.GetBeginAtomIdx()
-        j = b.GetEndAtomIdx()
-        if ((stereo_center_indexes.get(i) == True) or
-            (stereo_center_indexes.get(j) == True)):
-           b.SetBoolProp("protected", True)
-           log_protected_bond(name, b.GetIdx())
-        # protect bonds between stereo bond atoms and their stereo atoms
-        if b.GetStereo() != rdkit.Chem.rdchem.BondStereo.STEREONONE:
-            (k, l) = b.GetStereoAtoms()
-            b0 = mol.GetBondBetweenAtoms(i, k)
-            b1 = mol.GetBondBetweenAtoms(i, l)
-            b2 = mol.GetBondBetweenAtoms(j, k)
-            b3 = mol.GetBondBetweenAtoms(j, l)
-            if b0 != None:
-                b0.SetBoolProp("protected", True)
-                log_protected_bond(name, b0)
-            if b1 != None:
-                b1.SetBoolProp("protected", True)
-                log_protected_bond(name, b1)
-            if b2 != None:
-                b2.SetBoolProp("protected", True)
-                log_protected_bond(name, b2)
-            if b3 != None:
-                b3.SetBoolProp("protected", True)
-                log_protected_bond(name, b3)
-    res = []
-    for b in mol.GetBonds():
-        if ((b.GetBondType() == rdkit.Chem.rdchem.BondType.SINGLE) and
-            (not b.IsInRing()) and
-            (b.GetStereo() == rdkit.Chem.rdchem.BondStereo.STEREONONE) and
-            (b.HasProp("protected") == 0)): # HasProp returns an int... :(
-            res.append(b)
-    return res
-
 def print_typed_atoms(out, mol):
     stereo = get_atom_stereo_codes(mol)
     for a in mol.GetAtoms():
         i = a.GetIdx()
-        t = type_atom(a)
+        t = common.type_atom(a)
         s = stereo[i]
         print("%d %s,%d" % (i, t, s), file=out)
 
@@ -157,7 +85,7 @@ def char_of_bond_type(bond):
 
 def string_of_bond_stereo(bond):
     st = bond.GetStereo()
-    c = molenc_common.char_of_bond_stereo(st)
+    c = common.char_of_bond_stereo(st)
     if c == 'N':
         return c
     else:
@@ -178,7 +106,7 @@ def print_bonds(out, mol):
 
 # print which bonds are cuttable and the suggested number of cuts
 def print_cuttable_bonds(out, mol):
-    cuttable_bonds = find_cuttable_bonds(mol)
+    cuttable_bonds = common.find_cuttable_bonds(mol)
     total_weight = Descriptors.MolWt(mol)
     # 150 Da: D. Rognan's suggested max fragment weight
     nb_frags = round(total_weight / 150)
@@ -187,56 +115,6 @@ def print_cuttable_bonds(out, mol):
     for bond in cuttable_bonds:
         i = bond.GetIdx()
         print("%d" % i, file=out)
-
-def index_for_atom_type(atom_types_dict, atom_type):
-    try:
-        return atom_types_dict[atom_type]
-    except KeyError:
-        # want indexes to start at 1; so the isotope number is
-        # always explicit in the fragments SMILES output
-        v = len(atom_types_dict) + 1
-        atom_types_dict[atom_type] = v
-        return v
-
-def fragment_on_bonds_and_label(mol, bonds):
-    labels = []
-    dico = {}
-    for bi in bonds:
-        b = mol.GetBondWithIdx(bi)
-        i = b.GetBeginAtomIdx()
-        j = b.GetEndAtomIdx()
-        # get or create dictionary keys for those atom types
-        ai = mol.GetAtomWithIdx(i)
-        aj = mol.GetAtomWithIdx(j)
-        at_i = type_atom(ai)
-        at_j = type_atom(aj)
-        vi = index_for_atom_type(dico, at_i)
-        vj = index_for_atom_type(dico, at_j)
-        labels.append((vi, vj))
-    fragmented = Chem.FragmentOnBonds(mol, bonds, dummyLabels=labels)
-    smi = Chem.MolToSmiles(fragmented)
-    name = mol.GetProp("name")
-    return (smi, name, dico)
-
-# Smiling Surgeon-style SMILES fragmentation
-def cut_some_bonds(mol, seed):
-    cuttable_bonds = [b.GetIdx() for b in find_cuttable_bonds(mol)]
-    total_weight = Descriptors.MolWt(mol)
-    # 150 Da: D. Rognan's suggested max fragment weight
-    nb_frags = round(total_weight / 150)
-    max_cuts = min(len(cuttable_bonds), nb_frags - 1)
-    # print("mol %s; cut %d bonds" % (mol.GetProp("name"), max_cuts),
-    #       file=sys.stderr)
-    random.shuffle(cuttable_bonds)
-    to_cut = cuttable_bonds[0:max_cuts]
-    if len(to_cut) == 0:
-        # molecule too small: not fragmented
-        smi = Chem.MolToSmiles(mol)
-        name = mol.GetProp("name")
-        dico = {}
-        return (smi, name, dico)
-    else:
-        return fragment_on_bonds_and_label(mol, to_cut)
 
 if __name__ == '__main__':
     before = time.time()
@@ -250,14 +128,6 @@ if __name__ == '__main__':
     parser.add_argument("--draw", dest = "draw_mol", action ='store_true',
                         default = False,
                         help = "output PNG for each molecule w/ atom indexes")
-    parser.add_argument("--smisur", dest = "surgeon", action ='store_true',
-                        default = False, help = "Smiling Surgeon tests")
-    parser.add_argument("--seed", dest = "seed", default = 1234,
-                        type = int, help = "RNG seed")
-    parser.add_argument("-n", dest = "nb_passes", default = 1,
-                        type = int, help = "number of fragmentation passes")
-    parser.add_argument("--assemble", dest = "assemble", default = False,
-                        help = "assemble instead of fragmenting")
     # parse CLI
     if len(sys.argv) == 1:
         # user has no clue of what to do -> usage
@@ -266,39 +136,24 @@ if __name__ == '__main__':
     args = parser.parse_args()
     input_fn = args.input_fn
     draw_mol = args.draw_mol
-    nb_passes = args.nb_passes
-    smiles_surgeon_mode = args.surgeon
-    assemble = args.assemble
-    rng_seed = args.seed
-    random.seed(rng_seed)
     output = open(args.output_fn, 'w')
     count = 0
-    if assemble: # assembling fragments
-        assert(False)
-    else:
-        # fragmenting
-        mol_supplier = RobustSmilesMolSupplier(input_fn)
-        for name, mol in mol_supplier:
-            if smiles_surgeon_mode:
-                # Smiling Surgeon tests ---------
-                for i in range(nb_passes):
-                    fragments_smi, parent_name, dico = cut_some_bonds(mol, rng_seed)
-                    print("%s\t%s_p%d;%s" %
-                          (fragments_smi, name, i, str(dico)), file=output)
-            else: # previous --------------------
-                print("#atoms:%d %s" % (mol.GetNumAtoms(), name), file=output)
-                print_typed_atoms(output, mol)
-                print_bonds(output, mol)
-                print_cuttable_bonds(output, mol)
-            count += 1
-            if draw_mol:
-                d = rdMolDraw2D.MolDraw2DCairo(500, 500)
-                d.drawOptions().addAtomIndices = True
-                d.DrawMolecule(mol)
-                d.FinishDrawing()
-                png_fn = '%s.png' % name
-                with open(png_fn, 'wb') as fn:
-                    fn.write(d.GetDrawingText())
+    # fragmenting ---------------------------------------------------------
+    mol_supplier = RobustSmilesMolSupplier(input_fn)
+    for name, mol in mol_supplier:
+        print("#atoms:%d %s" % (mol.GetNumAtoms(), name), file=output)
+        print_typed_atoms(output, mol)
+        print_bonds(output, mol)
+        print_cuttable_bonds(output, mol)
+        count += 1
+        if draw_mol:
+            d = rdMolDraw2D.MolDraw2DCairo(500, 500)
+            d.drawOptions().addAtomIndices = True
+            d.DrawMolecule(mol)
+            d.FinishDrawing()
+            png_fn = '%s.png' % name
+            with open(png_fn, 'wb') as fn:
+                fn.write(d.GetDrawingText())
     after = time.time()
     dt = after - before
     print("%d molecules at %.2f mol/s" % (count, count / dt), file=sys.stderr)
