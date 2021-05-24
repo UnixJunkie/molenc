a5b68547aa2e3a050cddddce789ae6d03b7c0e2e
Francois Berenger <unixjunkie@sdf.org>
2021-05-24 11:21:20 +0900

    bin/molenc_deepsmi.py: WIP

--- bin/molenc_deepsmi.py
+++ bin/molenc_deepsmi.py
@@ -1,59 +1,57 @@
 #!/usr/bin/env python3
 
 # Copyright (C) 2021, Francois Berenger
-# Yamanishi laboratory,
-# Department of Bioscience and Bioinformatics,
-# Faculty of Computer Science and Systems Engineering,
-# Kyushu Institute of Technology,
-# 680-4 Kawazu, Iizuka, Fukuoka, 820-8502, Japan.
+# Tsuda laboratory, Tokyo University, Japan.
 
-# Implementation of
-# "Positional Analogue Scanning: An Effective Strategy for
-# Multiparameter Optimization in Drug Design".
-# Pennington, L. D., Aquila, B. M., Choi, Y., Valiulin, R. A., & Muegge, I.
-# Journal of medicinal chemistry (2020).
-# https://doi.org/10.1021/acs.jmedchem.9b02092
+# DeepSMILES encoder/decoder from/to SMILES
+#
+# "DeepSMILES: An adaptation of SMILES for use in machine-learning of
+# chemical structures". Noel M. O’Boyle and Andrew Dalke. ChemRxiv (2018).
 
 import argparse
+import deepsmiles
+import molenc_common
 import rdkit
+import sys
 import time
-import random
+
 from rdkit import Chem
 from rdkit.Chem import AllChem
-import sys
 
 from molenc_common import RobustSmilesMolSupplier
 
-def positional_analog_scan(mol, smarts_patt = '[cH]',
-                           smi_substs = ['N','CF','CC','CO',
-                                         'CCN','CCl','CC(F)(F)(F)','COC']):
-    res = []
-    ss = set() # a string set
-    patt = Chem.MolFromSmarts(smarts_patt)
-    for smi in smi_substs:
-        subst = Chem.MolFromSmiles(smi)
-        analogs = AllChem.ReplaceSubstructs(mol, patt, subst)
-        for a in analogs:
-            analog_smi = Chem.MolToSmiles(a) # canonicalization
-            # remove duplicates
-            if analog_smi not in ss:
-                res.append(analog_smi)
-                ss.add(analog_smi)
-    return res
+def encode(converter, smi):
+    return converter.encode(smi)
+
+def decode(converter, deep_smi):
+    try:
+        return converter.decode(deep_smi)
+    except deepsmiles.DecodeError as e:
+        print("molenc_deepsmi.py: decode: '%s'" % e.message,
+              file = sys.stderr)
+        return None
 
 if __name__ == '__main__':
     before = time.time()
     # CLI options
     parser = argparse.ArgumentParser(
-        description = "Positional Analog Scanning of each input molecule")
+        description = "DeepSMILES encoder/decoder")
     parser.add_argument("-i", metavar = "input.smi", dest = "input_fn",
                         help = "molecules input file")
     parser.add_argument("-o", metavar = "output.smi", dest = "output_fn",
-                        help = "analogs output file")
-    parser.add_argument("--rand-one", dest = "rand_one", action = "store_true",
+                        help = "molecules output file")
+    parser.add_argument("--no-rings", dest = "rings", action = "store_true",
                         default = False,
-                        help = "output only one randomly-chosen analog \
-                        per input molecule")
+                        help = "DeepSMILES without ring openings")
+    parser.add_argument("--no-branches", dest = "branches", action = "store_true",
+                        default = False,
+                        help = "DeepSMILES without branches")
+    parser.add_argument("-e", dest = "encode", action = "store_true",
+                        default = True,
+                        help = "encode: SMILES to DeepSMILES (default)")
+    parser.add_argument("-d", dest = "decode", action = "store_true",
+                        default = False,
+                        help = "decode: DeepSMILES to SMILES")
     # parse CLI ----------------------------------------------
     if len(sys.argv) == 1:
         # user has no clue of what to do -> usage
@@ -61,23 +59,30 @@ if __name__ == '__main__':
         sys.exit(1)
     args = parser.parse_args()
     input_fn = args.input_fn
-    rand_one = args.rand_one
     output = open(args.output_fn, 'w')
+    rings = args.rings
+    branches = args.branches
+    encode = args.encode
+    decode = args.decode
+    if encode and decode:
+        print("use either -e or -d, not both", file=sys.stderr)
+    if (not rings) and (not branches):
+        print("use at least --no-rings or --no-branches", file=sys.stderr)
     count = 0
     # work ----------------------------------------------
-    mol_supplier = RobustSmilesMolSupplier(input_fn)
-    for name, mol in mol_supplier:
-        analogs = positional_analog_scan(mol)
-        if rand_one:
-            l = list(analogs)
-            ana_smi = random.choice(l)
-            print("%s\t%s_ANA%03d" % (ana_smi, name, 0),
-                  file=output)
-        else: # print them all
-            for i, ana_smi in enumerate(analogs):
-                print("%s\t%s_ANA%03d" % (ana_smi, name, i),
-                      file=output)
-        count += 1
+    converter = deepsmiles.Converter(rings, branches)
+    smi_supplier = RobustSmilesSupplier(input_fn)
+    if encode:
+        for smi, name in smi_supplier:
+            deep_smi = encode(converter, smi)
+            print("%s\t%s" % (deep_smi, name), file=output)
+            count += 1
+    else:
+        for deep_smi, name in smi_supplier:
+            smi = decode(converter, deep_smi)
+            if smi != None:
+                print("%s\t%s" % (smi, name), file=output)
+            count += 1
     after = time.time()
     dt = after - before
     print("%d molecules at %.2f mol/s" % (count, count / dt), file=sys.stderr)
