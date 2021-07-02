c958886951194b8726c87820600ca39ce5cfa1c3
Francois Berenger <unixjunkie@sdf.org>
2021-07-02 12:16:37 +0900

    Indexer: handle several input files

--- src/indexer.ml
+++ src/indexer.ml
@@ -69,13 +69,20 @@ let main () =
       eprintf "usage:\n\
                %s\n  \
                -i <filename>: molecules input file\n  \
+               -ifs <filename>: file containing a list of files\n  \
                -np <int>: nprocs (default=1)\n  \
                -c <int>: chunk size (molecules/bloc)\n  \
                [-v]: verbose mode\n"
         Sys.argv.(0);
       exit 1
     end;
-  let input_fn = CLI.get_string ["-i"] args in
+  let input_fns =
+    match (CLI.get_string_opt ["-i"] args,
+           CLI.get_string_opt ["-ifs"] args) with
+    | (None, None)
+    | (Some _, Some _) -> failwith "provide either -i or -ifs"
+    | (Some fn, None) -> [fn]
+    | (None, Some fn) -> LO.lines_of_file fn in
   let nprocs = CLI.get_int_def ["-np"] args 1 in
   let csize = CLI.get_int_def ["-c"] args 50_000 in
   if CLI.get_set_bool ["-v"] args then
@@ -83,12 +90,14 @@ let main () =
   CLI.finalize (); (* ------------------------------------------------------ *)
   let chunk_count = ref 0 in
   let in_mol_count = ref 0 in
-  Log.info "%d molecules in %s" (LO.count input_fn) input_fn;
-  LO.with_in_file input_fn (fun input ->
-      Parany.run nprocs
-        ~demux:(read_one_chunk input_fn in_mol_count chunk_count csize input)
-        ~work:(index_one_chunk input_fn)
-        ~mux:(fun () -> ())
-    )
+  L.iter (fun input_fn ->
+      Log.info "%d molecules in %s" (LO.length input_fn) input_fn;
+      LO.with_in_file input_fn (fun input ->
+          Parany.run nprocs
+            ~demux:(read_one_chunk input_fn in_mol_count chunk_count csize input)
+            ~work:(index_one_chunk input_fn)
+            ~mux:(fun () -> ())
+        )
+    ) input_fns
 
 let () = main ()
