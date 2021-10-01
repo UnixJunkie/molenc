208e1b50e8ed064e1914673a330bd5588fb11c4a
Francois Berenger <unixjunkie@sdf.org>
2021-10-01 13:08:32 +0900

    Pareto: WIP

--- src/pareto.ml
+++ src/pareto.ml
@@ -15,6 +15,43 @@ let parse_field_nums s =
      the 1st index is 0 *)
   L.map (fun num_str -> (int_of_string num_str) - 1) strings
 
+type solution = float list
+
+let sum_scores (s: solution): float =
+  L.fsum s
+
+let sum_scores_decr_sort solutions =
+  L.sort (fun s1 s2 ->
+      BatFloat.compare (sum_scores s2) (sum_scores s1)
+    ) solutions
+
+let is_dominated (s1: solution) (s2: solution): bool =
+  L.for_all2 (fun p1 p2 -> p2 >= p1) s1 s2
+
+let is_dominated_by_any candidate competitors =
+  L.exists (is_dominated candidate) competitors
+
+let remove_dominated candidate competitors =
+  L.filter (fun competitor ->
+      not (is_dominated competitor candidate)
+    ) competitors
+
+let pareto_front solutions =
+  (* decr. sum scores sort *)
+  let sorted = sum_scores_decr_sort solutions in
+  (* loop until no dominated solution is left *)
+  let rec loop acc = function
+    | [] -> L.rev acc
+    | x :: xs ->
+      let xs' = remove_dominated x xs in
+      let acc' =
+        if is_dominated_by_any x xs' then
+          acc
+        else
+          x :: acc in
+      loop acc' xs' in
+  loop [] sorted
+
 let main () =
   Log.(set_log_level INFO);
   Log.color_on ();
@@ -30,8 +67,10 @@ let main () =
       exit 1
     end;
   let input_fn = CLI.get_string ["-i"] args in
-  (* ignore comment lines (starting with a '#') *)
-  let _all_lines = LO.filter input_fn (fun l -> not (S.starts_with l "#")) in
+  let _all_lines_uniq =
+    L.unique_cmp ~cmp:S.compare
+      (* ignore comment lines (starting with '#') *)
+      (LO.filter input_fn (fun l -> not (S.starts_with l "#"))) in
   let _sep = CLI.get_char_def ["-d"] args '\t' in
   let field_nums_str = CLI.get_string ["-f"] args in
   CLI.finalize();
