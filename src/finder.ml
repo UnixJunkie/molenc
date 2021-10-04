09a7b4fdb02ed32943a4085ee27b08fd0a0fb669
Francois Berenger <unixjunkie@sdf.org>
2021-10-04 15:49:45 +0900

    Finder: WIP

--- src/finder.ml
+++ src/finder.ml
@@ -1,7 +1,12 @@
 (* Copyright (C) 2021, Francois Berenger
-   Tsuda laboratory, Tokyo university, Japan.
+   Tsuda laboratory,
+   Graduate School of Frontier Sciences,
+   The University of Tokyo,
+   5-1-5 Kashiwa-no-ha,
+   Kashiwa, Chiba 277-8561, Japan.
 
-   Find name, Tanimoto-score and SMILES of nearest neighbor molecules. *)
+   Find name, Tanimoto-score and SMILES of nearest neighbor molecules.
+   Output a valid SMILES file. *)
 
 open Printf
 
@@ -13,50 +18,49 @@ module Ht = Hashtbl
 module L = BatList
 module LO = Line_oriented
 module Log = Dolog.Log
+module S = BatString
 module Utls = Molenc.Utls
+
+let split_smiles_line l =
+  (* Expect '\t' separated SMILES *)
+  S.split l ~by:"\t"
+
+let process_smiles_file fn name2smi =
+  LO.iteri fn (fun i line ->
+      let smi, name = split_smiles_line line in
+      (if Ht.mem name2smi name then
+         let () = Log.fatal "Finder.process_smiles_file: \
+                             already seen molecule name: %s" name in
+         exit 1
+       else
+         Ht.add name2smi name smi
+      );
+      if (i mod 1000) = 0 then
+        printf "Loaded molecules: %d\r%!" (Ht.length name2smi)
+    )
 
 let main () =
   Log.(set_log_level INFO);
   Log.color_on ();
   let argc, args = CLI.init () in
-  let default_block_size = ref 50_000 in
   if argc = 1 then
     begin
       eprintf "usage:\n\
                %s\n  \
-               -i <filename>: molecules input file\n  \
-               -ifs <filename>: file containing a list of files\n  \
-               -np <int>: nprocs (default=1)\n  \
-               -c <int>: chunk size (molecules/bloc; default=%d)\n  \
-               [-v]: verbose mode\n"
-        Sys.argv.(0) !default_block_size;
+               -i <filename.AP>: encoded molecules input file\n  \
+               --bst-fns <fn1[,fn2[,fn3...]]>: list of BST index files\n  \
+               --smi-fns <fn1[,fn2[,fn3...]]>: list of SMILES files\n  \
+               -np <int>: nprocs (default=1)\n"
+        Sys.argv.(0);
       exit 1
     end;
-  let input_fns =
-    match (CLI.get_string_opt ["-i"] args,
-           CLI.get_string_opt ["-ifs"] args) with
-    | (None, None)
-    | (Some _, Some _) -> failwith "provide either -i or -ifs"
-    | (Some fn, None) -> [fn]
-    | (None, Some fn) -> LO.lines_of_file fn in
+  let input_fn = CLI.get_string ["-i"] args in
+  let bst_fns = CLI.get_string ["--bst-fns"] args in
+  let smi_fns = CLI.get_string ["--smi-fns"] args in
   let nprocs = CLI.get_int_def ["-np"] args 1 in
-  let csize = CLI.get_int_def ["-c"] args !default_block_size in
-  if CLI.get_set_bool ["-v"] args then
-    verbose := true;
   CLI.finalize (); (* ------------------------------------------------------ *)
-  let chunk_count = ref 0 in
   let in_mol_count = ref 0 in
-  L.iter (fun input_fn ->
-      Log.info "%d molecules in %s" (LO.length input_fn) input_fn;
-      LO.with_in_file input_fn (fun input ->
-          Parany.run nprocs
-            ~demux:(read_one_chunk input_fn in_mol_count chunk_count csize input)
-            ~work:(index_one_chunk input_fn)
-            ~mux:(fun () -> ())
-        )
-    ) input_fns
+  (* MolIndex.nearest_neighbor_names_a ncores bst_fns to_annotate *)
+  failwith "not implemented yet"
 
 let () = main ()
-
-(* MolIndex.nearest_neighbor_names_a ncores bst_fns to_annotate *)
-  
