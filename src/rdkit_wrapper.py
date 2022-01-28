d10de9d52567748bcd11284eaf7b9978138917e3
Francois Berenger <unixjunkie@sdf.org>
2022-01-28 15:27:32 +0900

    modified:   rdkit_wrapper.py
    pyml_bindgen doesn't support tuples

--- src/rdkit_wrapper.py
+++ src/rdkit_wrapper.py
@@ -5,39 +5,37 @@ from rdkit import Chem
 class Rdkit:
     # this is needed because the OCaml side want to know how
     # to get an object of type t
-    def init(self, smi):
+    def __init__(self, smi):
         self.mol = Chem.MolFromSmiles(smi)
+        self.mat = Chem.GetDistanceMatrix(self.mol)
 
-    # # @staticmethod
-    # # def mol_from_smiles(smi):
-    # #     return Chem.MolFromSmiles(smi)
+    @staticmethod
+    def nb_heavy_atom_neighbors(a):
+        res = 0
+        for neighb in a.GetNeighbors():
+            if neighb.GetAtomicNum() > 1:
+                res += 1
+        return res
 
-    # @staticmethod
-    # def nb_heavy_atom_neighbors(a):
-    #     res = 0
-    #     for neighb in a.GetNeighbors():
-    #         if neighb.GetAtomicNum() > 1:
-    #             res += 1
-    #     return res
+    # return (#HA, #H)
+    @staticmethod
+    def count_neighbors(a):
+        nb_heavy = nb_heavy_atom_neighbors(a)
+        nb_H = a.GetTotalNumHs()
+        return (nb_heavy, nb_H)
+    
+    # (atomic_num, #HA, #H, valence - #H, formal_charge)
+    def type_atom(self, i):
+        a = self.mol.GetAtomWithIdx(i)
+        anum = a.GetAtomicNum()
+        assert(anum > 1) # we want to consider only heavy atoms
+        nb_HA, nb_H = count_neighbors(a)
+        valence = a.GetTotalValence()
+        HA_used_val = valence - nb_H
+        formal_charge = a.GetFormalCharge()
+        return [anum, nb_HA, nb_H, HA_used_val, formal_charge]
 
-    # # return (#HA, #H)
-    # @staticmethod
-    # def count_neighbors(a):
-    #     nb_heavy = nb_heavy_atom_neighbors(a)
-    #     nb_H = a.GetTotalNumHs()
-    #     return (nb_heavy, nb_H)
-
-    # # (atomic_num, #HA, #H, valence - #H, formal_charge)
-    # @staticmethod
-    # def type_atom(a):
-    #     anum = a.GetAtomicNum()
-    #     assert(anum > 1) # we want to consider only heavy atoms
-    #     nb_HA, nb_H = count_neighbors(a)
-    #     valence = a.GetTotalValence()
-    #     HA_used_val = valence - nb_H
-    #     formal_charge = a.GetFormalCharge()
-    #     return (anum, nb_HA, nb_H, HA_used_val, formal_charge)
-
+    # # pyml_bindgen doesn't support list of tuples or even tuples...
     # # type each atom of the molecule
     # def type_atoms(self):
     #     res = []
@@ -45,16 +43,12 @@ class Rdkit:
     #         res.append(type_atom(a))
     #     return res
 
-    # # get the distance (in bonds) between each pair of atom
-    # def get_distances(self):
-    #     mat = Chem.GetDistanceMatrix(self.mol)
-    #     nb_atoms = self.mol.GetNumAtoms()
-    #     res = []
-    #     for i in range(nb_atoms):
-    #         for j in range(i + 1, nb_atoms):
-    #             dist = int(mat[i][j])
-    #             res.append((i, j, dist))
-    #     return res
+    def get_num_atoms(self):
+        return self.mol.GetNumAtoms()
+
+    # get the distance (in bonds) between a pair of atoms
+    def get_distances(self, i, j):
+        return int(self.mat[i][j])
 
 # # tests
 # m = Chem.MolFromSmiles('c1ccccc1')
