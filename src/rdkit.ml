baf5409c548b8e97ec51ffd705d23016d698b510
Francois Berenger <unixjunkie@sdf.org>
2022-01-28 15:31:49 +0900

    modified:   rdkit.ml
    regenerated by ./gen_bindings.sh

--- src/rdkit.ml
+++ src/rdkit.ml
@@ -5,7 +5,13 @@ val of_pyobject : Pytypes.pyobject -> t option
 
 val to_pyobject : t -> Pytypes.pyobject
 
-val __init__ : smi:string -> unit -> t
+val __init__: smi:string -> unit -> t
+
+val type_atom: t -> i:int -> unit -> int array
+
+val get_num_atoms: t -> unit -> int
+
+val get_distance: t -> i:int -> j:int -> unit -> int
 
 end = struct
 let filter_opt l = List.filter_map Fun.id l
@@ -25,5 +31,11 @@ let of_pyobject pyo = if is_instance pyo then Some pyo else None
 let to_pyobject x = x
 
 let __init__ ~smi () = let callable = Py.Module.get (import_module ()) "Rdkit_wrapper" in let kwargs = filter_opt [ Some ("smi", Py.String.of_string smi); ] in of_pyobject @@ Py.Callable.to_function_with_keywords callable [||] kwargs
+
+let type_atom t ~i () = let callable = Py.Object.find_attr_string t "type_atom" in let kwargs = filter_opt [ Some ("i", Py.Int.of_int i); ] in Py.List.to_array_map Py.Int.to_int @@ Py.Callable.to_function_with_keywords callable [||] kwargs
+
+let get_num_atoms t () = let callable = Py.Object.find_attr_string t "get_num_atoms" in let kwargs = filter_opt [ ] in Py.Int.to_int @@ Py.Callable.to_function_with_keywords callable [||] kwargs
+
+let get_distance t ~i ~j () = let callable = Py.Object.find_attr_string t "get_distance" in let kwargs = filter_opt [ Some ("i", Py.Int.of_int i); Some ("j", Py.Int.of_int j); ] in Py.Int.to_int @@ Py.Callable.to_function_with_keywords callable [||] kwargs
 
 end
