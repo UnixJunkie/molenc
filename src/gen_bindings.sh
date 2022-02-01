c4e929dcd75754a6351168dd15790f4944591e96
Francois Berenger <unixjunkie@sdf.org>
2022-02-01 11:58:20 +0900

    src/gen_bindings.sh: auto-format the generated code
    using ocamlformat

--- src/gen_bindings.sh
+++ src/gen_bindings.sh
@@ -2,3 +2,5 @@
 
 pyml_bindgen rdkit_wrapper_specs.txt rdkit_wrapper Rdkit \
              --caml-module=Rdkit --of-pyo-ret-type=no_check > rdkit.ml
+# format the generated code
+ocamlformat --inplace --enable-outside-detected-project rdkit.ml
