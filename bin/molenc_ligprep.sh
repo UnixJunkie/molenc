a69df41dd7fc36af0ff030def06b7506516c34a3
Francois Berenger <unixjunkie@sdf.org>
2022-04-06 18:20:24 +0900

    modified:   bin/molenc_ligprep.sh
    high-accuracy mode also working

--- bin/molenc_ligprep.sh
+++ bin/molenc_ligprep.sh
@@ -16,10 +16,31 @@ PROTONATED=${OUT}_taut74.smi
 CONFORMER=${OUT}_taut74_1conf.sdf
 CHARGED=${OUT}_taut74_1conf_mmff.mol2
 
-# Default: fast mode ----------------------------------------------------------
+# # Default: fast mode --------------------------------------------------------
+
+# # protonation state at physiological pH
+# obabel $IN -O $PROTONATED -p 7.4
+
+# # lowest energy conformer
+# ~/usr/openeye/arch/Ubuntu-18.04-x64/omega/omega2 \
+#     -strictstereo false -maxconfs 1 -in $PROTONATED -out $CONFORMER
+
+# # assign partial charges
+# ~/usr/openeye/arch/Ubuntu-18.04-x64/quacpac/molcharge \
+#     -method mmff -in $CONFORMER -out $CHARGED
+
+# if user really want: accurate model -----------------------------------------
+# FBR: control this via a CLI option like --high
+
+PROTONATED=${OUT}_taut74.sdf
+CONFORMER=${OUT}_taut74_1conf.sdf
+CHARGED=${OUT}_taut74_1conf_am1bcc.mol2
 
 # protonation state at physiological pH
-obabel $IN -O $PROTONATED -p 7.4
+cp $IN $OUT.smiles # chemaxon cxcalc requires .smiles file extension...
+# -g --> skip erroneous molecules
+cxcalc -g majortautomer -H 7.4 -f sdf $OUT.smiles > $PROTONATED
+rm -f $OUT.smiles
 
 # lowest energy conformer
 ~/usr/openeye/arch/Ubuntu-18.04-x64/omega/omega2 \
@@ -27,7 +48,9 @@ obabel $IN -O $PROTONATED -p 7.4
 
 # assign partial charges
 ~/usr/openeye/arch/Ubuntu-18.04-x64/quacpac/molcharge \
-    -method mmff -in $CONFORMER -out $CHARGED
+    -method am1bcc -in $CONFORMER -out $CHARGED
+
+# cleanup ---------------------------------------------------------------------
 
 # rm temp. files
 rm -f $PROTONATED $CONFORMER
@@ -35,6 +58,3 @@ rm -f $PROTONATED $CONFORMER
 # user feedback
 wc -l $IN
 grep -c '@<TRIPOS>MOLECULE' $CHARGED
-
-# if user really want: accurate model -----------------------------------------
-# TODO
