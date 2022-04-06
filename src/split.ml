437610081a96dc5a643b8423330c1e8bdf8e8d41
Francois Berenger <unixjunkie@sdf.org>
2022-04-06 14:44:59 +0900

    Split: OK for tests

--- src/split.ml
+++ src/split.ml
@@ -20,37 +20,71 @@ type file_format = SMILES
                  | Mol2
 
 let file_format_of_filename fn =
-  if S.ends_with fn ".smi" || S.ends_with fn ".smiles" then
-    SMILES
+  if S.ends_with fn ".smi" then
+    (SMILES, ".smi")
+  else if S.ends_with fn ".smiles" then
+    (SMILES, ".smiles")
   else if S.ends_with fn ".sdf" then
-    SDF
+    (SDF, ".sdf")
   else if S.ends_with fn ".mol2" then
-    Mol2
+    (Mol2, ".mol2")
   else
     failwith
       ("Split.file_format_of_filename: not {.smi|.smiles|.sdf|.mol2}: %s" ^ fn)
 
+(* molecule separators *)
+let mol2_header = "@<TRIPOS>MOLECULE"
+let sdf_footer = "$$$$"
+
 exception Read_one
 
-let read_one ff input =
+let read_one ff count input =
   match ff with
-  | Mol2 -> failwith "not implemented yet"
-  | SMILES -> [input_line input]
+  | Mol2 ->
+    begin
+      let first_line = input_line input in
+      incr count;
+      let res =
+        if first_line = mol2_header then
+          ref [mol2_header]
+        else
+          (* add back mol2 header from previous molecule read *)
+          ref [first_line; mol2_header] in
+      try
+        while true do
+          let line = input_line input in
+          if line = mol2_header then
+            raise Read_one (* consumed the mol2 header from next molecule *)
+          else
+            res := line :: !res
+        done;
+        assert(false)
+      with End_of_file | Read_one -> L.rev !res
+    end
+  | SMILES ->
+    begin
+      try
+        let line = input_line input in
+        incr count;
+        [line]
+      with End_of_file -> []
+    end
   | SDF ->
     let res = ref [] in
     try
       while true do
         let line = input_line input in
-        if line = "$$$$" then
+        if line = sdf_footer then
           begin
             res := line :: !res;
+            incr count;
             raise Read_one
           end
         else
           res := line :: !res;
       done;
       assert(false)
-    with Read_one -> L.rev !res
+    with Read_one | End_of_file -> L.rev !res
 
 let main () =
   Log.color_on ();
@@ -59,20 +93,27 @@ let main () =
   if argc = 1 then
     (eprintf "usage:\n  \
               %s\n  \
-              [-i <filename>]: SMILES input file\n  \
-              -o <filename>: output file\n  \
-              [-np <int>]: nprocs (default=1)\n  \
-              [--bbad <fn1[,fn2[,...]]>]: previously computed BBAD\n  \
-              to apply as filter\n  \
-              will compute the union of ADs if several filenames\n  \
-              [-v]: verbose/debug mode\n" Sys.argv.(0);
+              -i <filename.{smi|mol2|sdf}>: molecules input file\n  \
+              [-c <int>]: chunk size (molecules per output file; default=50)\n"
+       Sys.argv.(0);
      exit 1);
-  let verbose = CLI.get_set_bool ["-v"] args in
-  if verbose then Log.(set_log_level DEBUG);
-  let input_fn = CLI.get_string_def ["-i"] args "/dev/stdin" in
-  let _csize = CLI.get_int_def ["-c"] args 50 in
+  let input_fn = CLI.get_string ["-i"] args in
+  let csize = CLI.get_int_def ["-c"] args 50 in
   CLI.finalize ();
-  let _ff = file_format_of_filename input_fn in
-  failwith "not implemented yet"
+  let ff, ext = file_format_of_filename input_fn in
+  let basename = Filename.chop_suffix input_fn ext in
+  let count = ref 0 in
+  LO.with_in_file input_fn (fun input ->
+      let output_fn = sprintf "%s_%04d%s" basename !count ext in
+      LO.with_out_file output_fn (fun out ->
+          try
+            for _i = 1 to csize do
+              let mol = read_one ff count input in
+              L.iter (fprintf out "%s\n") mol
+            done
+          with End_of_file -> ()
+        )
+    );
+  Log.info "read %d from %s" !count input_fn
 
 let () = main ()
