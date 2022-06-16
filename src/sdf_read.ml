6dd702a5fc3ae77806b9b8bc2510cd6138987647
Francois Berenger <berenger@edu.k.u-tokyo.ac.jp>
2022-06-16 16:25:30 +0200

    Sdf_read: fix compilation

--- src/sdf_read.ml
+++ src/sdf_read.ml
@@ -6,13 +6,14 @@
    Dump a .sdf file in txt format. *)
 
 module LO = Line_oriented
+module Sdf_3D = Molenc.Sdf_3D
 
 let main () =
   let input_fn = Sys.argv.(1) in
   LO.with_in_file input_fn (fun input ->
       try
         while true do
-          let mol = Sdf_3D.read_one_molecule input in
+          let _mol = Sdf_3D.read_one_molecule input in
           ()
         done
       with End_of_file -> ()
