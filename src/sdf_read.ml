abcd484b1c0faab855664cad9e4d10c0eed68789
Francois Berenger <berenger@edu.k.u-tokyo.ac.jp>
2022-06-16 17:13:46 +0200

    Sdf_read: print what was parsed in text format

--- src/sdf_read.ml
+++ src/sdf_read.ml
@@ -5,16 +5,28 @@
 
    Dump a .sdf file in txt format. *)
 
+open Printf
+
+module A = BatArray
 module LO = Line_oriented
 module Sdf_3D = Molenc.Sdf_3D
+module V3 = Vector3
 
 let main () =
   let input_fn = Sys.argv.(1) in
   LO.with_in_file input_fn (fun input ->
       try
         while true do
-          let _mol = Sdf_3D.read_one_molecule input in
-          ()
+          let mol = Sdf_3D.read_one_molecule input in
+          let name = Sdf_3D.(mol.name) in
+          let elts = Sdf_3D.(mol.elements) in
+          let coords = Sdf_3D.(mol.coords) in
+          printf "%s\n" name;
+          A.iter2 (fun xyz anum ->
+              let (x, y, z) = V3.to_triplet xyz in
+              let elt = Sdf_3D.symbol_of_anum anum in
+              printf "%f %f %f %s\n" x y z elt
+            ) coords elts
         done
       with End_of_file -> ()
     )
