1104ba24f6b137a9c5158ab38e368a0065b30169
Francois Berenger <berenger@edu.k.u-tokyo.ac.jp>
2022-06-17 14:40:45 +0200

    Sdf_3dae: need to test

--- src/sdf_3dae.ml
+++ src/sdf_3dae.ml
@@ -3,30 +3,59 @@
    Tsuda laboratory, Graduate School of Frontier Sciences,
    The University of Tokyo, Japan.
 
-   Dump a .sdf file in txt format. *)
+   Dump a 3D .sdf file's atoms using 3DAE. *)
 
 open Printf
 
 module A = BatArray
+module CLI = Minicli.CLI
 module LO = Line_oriented
+module Log = Dolog.Log
 module Sdf_3D = Molenc.Sdf_3D
 module V3 = Vector3
 
 let main () =
-  let input_fn = Sys.argv.(1) in
-  LO.with_in_file input_fn (fun input ->
+  Log.color_on ();
+  Log.(set_log_level INFO);
+  let argc, args = CLI.init () in
+  if argc = 1 then
+    (eprintf "usage:\n  \
+              %s\n  \
+              [-i <filename.sdf>]: input file\n  \
+              -o <filename.csv>: output file\n  \
+              [-np <int>]: nprocs (default=1)\n  \
+              [-v]: verbose/debug mode\n" Sys.argv.(0);
+     exit 1);
+  let verbose = CLI.get_set_bool ["-v"] args in
+  if verbose then Log.(set_log_level DEBUG);
+  let input_fn = CLI.get_string_def ["-i"] args "/dev/stdin" in
+  let output_fn = CLI.get_string ["-o"] args in
+  let _nprocs = CLI.get_int_def ["-np"] args 1 in
+  (* FBR: control those params through the CLI *)
+  let nb_layers = 1 in
+  let cutoff = 3.5 in
+  let dx = 0.1 in
+  CLI.finalize (); (* ------------------------------------------------------ *)
+  LO.with_infile_outfile input_fn output_fn (fun input output ->
       try
         while true do
           let mol = Sdf_3D.read_one_molecule input in
-          let name = Sdf_3D.(mol.name) in
-          let elts = Sdf_3D.(mol.elements) in
-          let coords = Sdf_3D.(mol.coords) in
-          printf "%s\n" name;
-          A.iter2 (fun xyz anum ->
-              let (x, y, z) = V3.to_triplet xyz in
-              let elt = Sdf_3D.symbol_of_anum anum in
-              printf "%g %g %g %s\n" x y z elt
-            ) coords elts
+          let atoms_3dae = Sdf_3D.encode_atoms nb_layers cutoff dx mol in
+          A.iteri (fun i_atom encoded_atom ->
+              fprintf output "%d" i_atom;
+              let nb_dx = A.length atoms_3dae in
+              let nb_chans = A.length (atoms_3dae.(0)) in
+              for i_chan = 0 to nb_chans - 1 do
+                for i_dx = 0 to nb_dx - 1 do
+                  let feat = encoded_atom.(i_dx).(i_chan) in
+                  if feat > 0.0 then
+                    (* the feature vector should be very sparse *)
+                    let feat_idx = i_dx + (i_chan * nb_dx) in
+                    fprintf output " %d:%g" feat_idx feat
+                done
+              done;
+              fprintf output "'n"
+            ) atoms_3dae
         done
       with End_of_file -> ()
     )
