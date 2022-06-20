18dbed151c1433b733a1b8dc12a742b70c73255f
Francois Berenger <berenger@edu.k.u-tokyo.ac.jp>
2022-06-20 15:27:25 +0200

    test_sdf_read.sh: set -x; also call the exe directly
    rather than assuming there is a soft link to it

--- test_sdf_read.sh
+++ test_sdf_read.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-#set -x # DEBUG
+set -x # DEBUG
 
 # check we parse correctly 10 3D molecules in a .sdf file
 rm -f data/chembl30_10mols.txt.curr
-./sdf_read data/chembl30_10mols.sdf > data/chembl30_10mols.txt.curr
+_build/default/src/sdf_read.exe data/chembl30_10mols.sdf \
+                                > data/chembl30_10mols.txt.curr
 diff data/chembl30_10mols.txt.ref data/chembl30_10mols.txt.curr
