d801364693c0cda278029c4329df2b3a1c727564
Francois Berenger <unixjunkie@sdf.org>
2023-04-03 16:42:45 +0900

    bin/molenc_rotbond.py: debugged
    have to use removeHs=True even if that's not what
    I want because of probable rdkit bug;
    if any H is listed before the end of the heavy atoms list,
    THIS WILL CHANGE ATOM NUMBERING !!!

--- bin/molenc_rotbond.py
+++ bin/molenc_rotbond.py
@@ -16,27 +16,27 @@
 import argparse, sys
 from rdkit import Chem
 
-def count_heavy_neighbors(a):
-    res = 0
-    for neighb in a.GetNeighbors():
-        if neighb.GetAtomicNum() != 1:
-            res += 1
-    return res
-
-# terminal heavy atom with attached H
-def is_hydrogenated_terminal(a):
-    return (a.GetAtomicNum() != 1 and         # not H
-            count_heavy_neighbors(a) == 1 and # terminal
-            a.GetTotalNumHs() >= 1)           # hydrogenated
-
-def already_protonated(mol0):
-    before = mol0.GetNumAtoms()
-    mol1 = Chem.AddHs(mol0)
-    after = mol1.GetNumAtoms()
+def already_protonated(mol):
+    before = mol.GetNumAtoms()
+    molH = Chem.AddHs(mol)
+    after = molH.GetNumAtoms()
     return (before == after)
 
 def is_HA(a):
     return (a.GetAtomicNum() != 1)
+
+def count_heavy_neighbors(a):
+    res = 0
+    for n in a.GetNeighbors():
+        if is_HA(n):
+            res += 1
+    return res
+
+# a terminal heavy atom with at least one hydrogen attached
+def is_hydrogenated_terminal(a):
+    return (is_HA(a) and \
+            count_heavy_neighbors(a) == 1 and \
+            a.GetTotalNumHs() >= 1)
 
 if __name__ == '__main__':
     # CLI options parsing
@@ -51,34 +51,39 @@ if __name__ == '__main__':
     args = parser.parse_args()
     input_fn = args.input_fn
     # parse CLI end -----------------------------------------------------------
-    mol_supplier = Chem.SDMolSupplier(input_fn, removeHs=False)
-    for mol in mol_supplier:
+    # BUG IF removeHs=False; hydrogens are not properfly counted attached
+    # to heavy atoms in this case !!!!!!!!!!!!!!!!
+    # REPORTED on ML
+    for mol in Chem.SDMolSupplier(input_fn, removeHs=True):
         if mol == None:
             assert(False)
+        # debug_mol(mol)
         name = mol.GetProp('_Name')
         if not already_protonated(mol):
+            # a conformer for docking should have explicit hydrogens
             print("ERROR: not protonated: %s" % name, file=sys.stderr)
-        else:
-            regular_bonds = []
-            movingH_bonds = []
-            for b in mol.GetBonds():
-                start_a = b.GetBeginAtom()
-                stop_a = b.GetEndAtom()
-                # we are only interested in single bonds between heavy atoms
-                # and out of rings
-                if is_HA(start_a) and is_HA(stop_a) and \
-                   b.GetBondTypeAsDouble() == 1.0 and not b.IsInRing():
-                    i = start_a.GetIdx()
-                    j = stop_a.GetIdx()
-                    ij = (i, j)
-                    if is_hydrogenated_terminal(start_a) or \
-                       is_hydrogenated_terminal(stop_a):
-                        movingH_bonds.append(ij)
-                    else:
-                        regular_bonds.append(ij)
-            total = len(regular_bonds) + len(movingH_bonds)
-            print('%d:%s' % (total, name))
-            for i, j in regular_bonds:
-                print("REG\t%d\t%d" % (i, j))
-            for i, j in movingH_bonds:
-                print("THA\t%d\t%d" % (i, j))
+            # sys.exit(1)
+        reg_bonds = []
+        ter_bonds = []
+        # examine each bond
+        for b in mol.GetBonds():
+            a_i = b.GetBeginAtom()
+            a_j = b.GetEndAtom()
+            i = a_i.GetIdx()
+            j = a_j.GetIdx()
+            ij = (i, j)
+            if is_HA(a_i) and is_HA(a_j) and \
+               b.GetBondTypeAsDouble() == 1.0 and \
+               not b.IsInRing():
+                if is_hydrogenated_terminal(a_i) or is_hydrogenated_terminal(a_j):
+                    ter_bonds.append(ij)
+                else:
+                    reg_bonds.append(ij)
+        num_reg_bonds = len(reg_bonds)
+        num_ter_bonds = len(ter_bonds)
+        total = num_reg_bonds + num_ter_bonds
+        print('%d:%d:%s' % (total, num_ter_bonds, name))
+        for i, j in reg_bonds:
+            print('REG\t%d\t%d' % (i, j))
+        for i, j in ter_bonds:
+            print('TER\t%d\t%d' % (i, j))
