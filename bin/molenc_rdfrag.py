9ef24563939188cc9104b3b6057cf41ab16c6078
Francois Berenger <unixjunkie@sdf.org>
2023-09-22 04:38:02 -0400

    modified:   bin/molenc_rdfrag.py

--- bin/molenc_rdfrag.py
+++ bin/molenc_rdfrag.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 #
 # Copyright (C) 2023, Francois Berenger
-# BRICS or RECAP molecular fragmentation
+# BRICS or RECAP molecular fragmentation using rdkit
+# (end-user command-line tool)
 
 import argparse
 import molenc_common as common
@@ -16,14 +17,17 @@ if __name__ == '__main__':
     before = time.time()
     # CLI options parsing
     parser = argparse.ArgumentParser(
-        description = "compute molecule fragmentation hints")
+        description = "fragment molecules using BRICS or RECAP")
     parser.add_argument("-i", metavar = "input.smi", dest = "input_fn",
                         help = "molecules input file")
-    parser.add_argument("-o", metavar = "output.txt", dest = "output_fn",
+    parser.add_argument("-o", metavar = "output.smi", dest = "output_fn",
                         help = "output file")
-    parser.add_argument("--draw", dest = "draw_mol", action ='store_true',
+    parser.add_argument("--brics", dest = "brics", action ='store_true',
                         default = False,
-                        help = "output PNG for each molecule w/ atom indexes")
+                        help = "use BRICS")
+    parser.add_argument("--recap", dest = "recap", action ='store_true',
+                        default = True,
+                        help = "use RECAP (default)")
     # parse CLI
     if len(sys.argv) == 1:
         # user has no clue of what to do -> usage
@@ -31,26 +35,15 @@ if __name__ == '__main__':
         sys.exit(1)
     args = parser.parse_args()
     input_fn = args.input_fn
-    draw_mol = args.draw_mol
-    output = open(args.output_fn, 'w')
+    output_fn = args.output_fn
     count = 0
     # fragmenting ---------------------------------------------------------
-    mol_supplier = RobustSmilesMolSupplier(input_fn)
-    for name, mol in mol_supplier:
-        print("#atoms:%d %s" % (mol.GetNumAtoms(), name), file=output)
-        print_typed_atoms(output, mol)
-        print_bonds(output, mol)
-        print_cuttable_bonds(output, mol)
-        count += 1
-        if draw_mol:
-            d = rdMolDraw2D.MolDraw2DCairo(500, 500)
-            d.drawOptions().addAtomIndices = True
-            d.DrawMolecule(mol)
-            d.FinishDrawing()
-            png_fn = '%s.png' % name
-            with open(png_fn, 'wb') as fn:
-                fn.write(d.GetDrawingText())
-    after = time.time()
-    dt = after - before
-    print("%d molecules at %.2f mol/s" % (count, count / dt), file=sys.stderr)
-    output.close()
+    with open(output_fn, 'w') as output:
+      mol_supplier = RobustSmilesMolSupplier(input_fn)
+      for name, mol in mol_supplier:
+          print("#atoms:%d %s" % (mol.GetNumAtoms(), name), file=output)
+          count += 1
+      after = time.time()
+      dt = after - before
+      print("fragmented %d molecules at %.2f mol/s" % (count, count / dt),
+            file=sys.stderr)
