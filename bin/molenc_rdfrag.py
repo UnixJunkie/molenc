fa2753961d48781bb7cd3030c19cdc13ab0ef6bf
Francois Berenger <unixjunkie@sdf.org>
2023-09-22 14:37:05 +0200

    bin/molenc_rdfrag.py: starting to be useful for RECAP

--- bin/molenc_rdfrag.py
+++ bin/molenc_rdfrag.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 #
 # Copyright (C) 2023, Francois Berenger
-# BRICS or RECAP molecular fragmentation using rdkit
-# a end-user command-line tool
+# BRICS or RECAP molecular fragmentation using rdkit (CLI tool)
 
 import argparse, rdkit, sys, time
 
@@ -12,7 +11,8 @@ from rdkit.Chem import Recap
 
 #TODO
 # - find cut bonds; remove them
-# - output fragments as a SMILES mixture; keep showing atomMapNums
+#   - for each fragment; check it is found only once
+# - output fragments as a SMILES mixture
 
 # REMARKS
 # FBR: RECAP: is there really a synthesis tree output? With layers?
@@ -24,18 +24,45 @@ def numerate_atoms(mol):
         a.SetAtomMapNum(i)
         i += 1
 
+# # substructure search
+# m.HasSubstructMatch(patt)
+# True
+# m.GetSubstructMatch(patt)
+# (0, 5, 6)
+# m.GetSubstructMatches(patt)
+# ((0, 5, 6), (4, 5, 6))        
+
 def fragment_RECAP(out, mol, name):
     hierarch = Recap.RecapDecompose(mol)
-    frags = hierarch.GetLeaves()
-    if len(frags) == 1:
+    frags = hierarch.GetLeaves().keys()
+    num_frags = len(frags)
+    print("fragments: %d" % num_frags)
+    if num_frags <= 1:
         print("could not fragment: %s" % Chem.MolToSmiles(mol),
               file=sys.stderr)
     else:
-        numerate_atoms(mol        )
+        numerate_atoms(mol)
         in_mol_smi = Chem.MolToSmiles(mol)
         print("%s\t%s" % (in_mol_smi, name), file=out)
-        # for smi in frags:
-        #     print(smi, file=out)
+        frag_idx = 1
+        for smi in frags:
+            assert(type(smi) == str)
+            patt = Chem.MolFromSmarts(smi)
+            matches = mol.GetSubstructMatches(patt)
+            num_matches = len(matches)
+            if num_matches == 0:
+                print("fragment not found: %s" % smi,
+                      file=sys.stderr)
+            if num_matches > 1:
+                print("fragment found several times: %s" % smi,
+                      file=sys.stderr)
+            for match in matches:
+                for i in match:
+                    a = mol.GetAtomWithIdx(i)
+                    a.SetAtomMapNum(frag_idx)
+                frag_idx += 1
+    res = Chem.MolToSmiles(mol)
+    print('%s\t%s_RECAP_fragments' % (res, name), file=out)
 
 if __name__ == '__main__':
     before = time.time()
