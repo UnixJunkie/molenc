ca1686af4d1e9ac8889a1856c8bccd524f731287
Francois Berenger <unixjunkie@sdf.org>
2023-11-28 12:50:34 +0900

    Molenc_AP: added --no-std CLI option

--- src/molenc_AP.ml
+++ src/molenc_AP.ml
@@ -56,6 +56,9 @@ let standardize_some tmp_smi_fn =
   Sys.remove tmp_smi_fn;
   tmp_std_smi_fn
 
+let do_not_standardize_some tmp_smi_fn =
+  tmp_smi_fn
+
 let catenate_some dst_fn tmp_src_fn =
   let cmd = sprintf "cat %s >> %s" tmp_src_fn dst_fn in
   (if !verbose then Log.debug "running: %s" cmd);
@@ -75,6 +78,7 @@ let main () =
                -i <input.smi>: input molecules\n  \
                -o <output.AP>: unfolded counted atom pairs output\n  \
                -d <dico.dix>: use existing feature dictionary\n  \
+               [--no-std]: do not standardize molecules\n  \
                [-m <int>]: maximum atom pairs distance (in bonds; default=OFF)\n  \
                [-np <int>]: parallelize on NCORES (default=1)\n  \
                [-cs <int>]: chunk size (default=50)\n  \
@@ -86,15 +90,21 @@ let main () =
   let output_fn = CLI.get_string ["-o"] args in
   let nprocs = CLI.get_int_def ["-np"] args 1 in
   let csize = CLI.get_int_def ["-cs"] args 50 in
+  let no_std = CLI.get_set_bool ["--no-std"] args in
   let _max_dist_opt = CLI.get_int_opt ["-m"] args in
   let _dict_mode = match CLI.get_string_opt ["-d"] args with
     | None -> Output_dict (input_fn ^ ".dix")
     | Some fn -> Input_dict fn in
   CLI.finalize (); (* ------------------------------------------------------ *)
+  let standardization =
+    if no_std then
+      do_not_standardize_some
+    else
+      standardize_some in
   LO.with_in_file input_fn (fun input ->
       Parany.run ~preserve:true nprocs
         ~demux:(fun () -> read_some csize input)
-        ~work:standardize_some
+        ~work:standardization
         ~mux:(catenate_some output_fn)
     )
 
