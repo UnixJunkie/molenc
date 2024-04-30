629915b5eedd651d7eeceff378f0afddef4f3675
Francois Berenger <unixjunkie@sdf.org>
2024-04-30 10:46:54 +0900

    bin/molenc_rfr.py: support training a classifier

--- bin/molenc_rfr.py
+++ bin/molenc_rfr.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
-# Copyright (C) 2022, Francois Berenger
+# Copyright (C) 2024, Francois Berenger
 # Tsuda laboratory, Graduate School of Frontier Sciences,
 # The University of Tokyo, Japan.
 #
-# Random Forests Regressor (RFR) wrapper script
+# Random Forests Regressor or Classifier (RFR or RFC) CLI wrapper
 
 import argparse
 import math
@@ -18,6 +18,7 @@ import time
 import joblib
 
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import r2_score, mean_squared_error
 from scipy import sparse
 
@@ -84,6 +85,29 @@ def read_AP_lines_regr(max_feature_index, lines):
     log('read_AP_lines_regr: (%d,%d)' % (X.shape[0], X.shape[1]))
     return (X, pIC50s)
 
+def label_of_name(name):
+    if name.startswith('active'):
+        return 1
+    else:
+        return 0
+
+def read_AP_lines_class(max_feature_index, lines):
+    nb_lines = len(lines)
+    names = []
+    labels = []
+    row = []
+    col = []
+    data = []
+    for i, line in enumerate(lines):
+        name, _pIC50, features_str = split_AP_line(line)
+        names.append(name)
+        labels.append(label_of_name(name))
+        read_features(row, col, data, i, max_feature_index, features_str)
+    X = sparse.csr_matrix((data, (row, col)),
+                          shape=(nb_lines, max_feature_index + 1))
+    log('read_AP_lines_class: (%d,%d)' % (X.shape[0], X.shape[1]))
+    return (names, X, labels)
+
 def train_test_split(train_portion, lines):
     n = len(lines)
     train_n = math.ceil(train_portion * n)
@@ -104,6 +128,18 @@ def train_regr(ntrees, crit, nprocs, msl, max_features, max_samples,
                                   min_samples_leaf = msl,
                                   max_features = max_features,
                                   max_samples = max_samples)
+    model.fit(X_train, y_train)
+    return model
+
+# WARNING: in binary classification, y_train must be only 0s and 1s
+def train_class(ntrees, nprocs, msl, max_features, X_train, y_train):
+    log('classifier training...')
+    model = RandomForestClassifier(n_estimators = ntrees,
+                                   criterion = 'gini',
+                                   n_jobs = nprocs,
+                                   oob_score = True,
+                                   min_samples_leaf = msl,
+                                   max_features = max_features)
     model.fit(X_train, y_train)
     return model
 
@@ -154,11 +190,43 @@ def train_test_NxCV_regr(max_feat, ntrees, crit, nprocs, msl, max_features,
         fold += 1
     return (truth, preds)
 
+def train_test_NxCV_class(max_feat, ntrees, nprocs, msl, max_features,
+                          all_lines, cv_folds):
+    truth = []
+    preds = []
+    fold = 0
+    train_tests = list_split(all_lines, cv_folds)
+    for train_set, test_set in train_tests:
+        _names, X_train, y_train = read_AP_lines_class(max_feat, train_set)
+        model = train_class(ntrees, nprocs, msl, max_features,
+                            X_train, y_train)
+        _names, X_test, y_ref = read_AP_lines_class(max_feat, test_set)
+        truth = truth + y_ref
+        y_preds = test(model, X_test)
+        y_preds_lst = []
+        for y in y_preds:
+            y_preds_lst.append(y)
+        auc = sklearn.metrics.roc_auc_score(y_ref, y_preds_lst)
+        mcc = sklearn.metrics.matthews_corrcoef(y_ref, y_preds_lst)
+        log('fold: %d AUC: %f MCC: %.3f' % (fold, auc, mcc))
+        preds = preds + y_preds_lst
+        fold += 1
+    return (truth, preds)
+
 def dump_pred_scores(output_fn, preds):
     if output_fn != '':
         with open(output_fn, 'w') as output:
             for pred in preds:
                 print('%f' % pred, file=output)
+
+# a prediction is an integer class label, not a float
+# FBR: maybe output the active class proba as an additional column
+def dump_pred_labels(output_fn, names, preds):
+    assert(len(names) == len(preds))
+    if output_fn != '':
+        with open(output_fn, 'w') as output:
+            for name_pred in zip(names, preds):
+                print('%s\t%d' % name_pred, file=output)
 
 def gnuplot(title0, actual_values, predicted_values):
     # escape underscores so that gnuplot doesn't interprete them
@@ -274,6 +342,11 @@ if __name__ == '__main__':
                         # keep None here, not 1.0; reported sklearn bug
                         default = None,
                         help = 'max samples per bootstrap')
+    parser.add_argument('--classif', dest = 'classification',
+                        action ='store_true',
+                        default = False,
+                        help = 'train a classifier \
+                        (default: train a regressor)')
     # parse CLI ---------------------------------------------------------
     if len(sys.argv) == 1:
         # user has no clue of what to do -> usage
@@ -316,6 +389,8 @@ if __name__ == '__main__':
     if max_samples == 1.0:
         max_samples = None # BUG in sklearn RFR probably; this forces 1.0
     no_compress = args.no_compress
+    classification = args.classification
+    regressor = not classification
     # work ---------------------------------------------------------
     # read in a .AP file w/ pIC50 values
     all_lines = lines_of_file(input_fn)
@@ -326,47 +401,79 @@ if __name__ == '__main__':
     model = None
     if cv_folds == 1:
         train_lines, test_lines = train_test_split(train_p, all_lines)
-        #regression
-        X_train, y_train = read_AP_lines_regr(max_feat, train_lines)
-        X_test, y_test = read_AP_lines_regr(max_feat, test_lines)
-        if model_input_fn != '':
-            log('loading model from %s' % model_input_fn)
-            model = joblib.load(model_input_fn)
-        else:
-            model = train_regr(ntrees, crit, nprocs, msl, max_features,
-                               max_samples, X_train, y_train)
-            if model_output_fn != '':
-                log('saving model to %s' % model_output_fn)
-                if no_compress:
-                    joblib.dump(model, model_output_fn, compress=False)
-                else:
-                    joblib.dump(model, model_output_fn, compress=3)
-        # predict w/ trained model
-        if train_p < 1.0:
-            y_preds = test(model, X_test)
-            dump_pred_scores(output_fn, y_preds)
-            r2 = r2_score(y_test, y_preds)
-            rmse = mean_squared_error(y_test, y_preds, squared=False)
-            if train_p > 0.0:
-                # train/test case
-                log('R2: %f RMSE: %f' % (r2, rmse))
+	if classification:
+            _names_train, X_train, y_train = \
+            	read_AP_lines_class(max_feat, train_lines)
+            names_test, X_test, y_test = \
+            	read_AP_lines_class(max_feat, test_lines)
+            if model_input_fn != '':
+    		log('load model from %s' % model_input_fn)
+                    model = joblib.load(model_input_fn)
             else:
-                # maybe production run or predictions
-                # on an external validation set
-                log('R2: %f RMSE: %f !!! ONLY VALID if test set had target values !!!' % (r2, rmse))
-    else: # cv_folds > 1
-        #regression
-            truth, preds = train_test_NxCV_regr(max_feat, ntrees, crit,
-                                                nprocs, msl, max_features,
-                                                max_samples, all_lines,
-                                                cv_folds)
-            log('truths: %d preds: %d' % (len(truth), len(preds)))
-            r2 = r2_score(truth, preds)
-            rmse = mean_squared_error(truth, preds, squared=False)
-            r2_rmse = 'R2=%.3f RMSE=%.4f' % (r2, rmse)
-            log(r2_rmse)
-            title = '%s N=%d folds=%d mtry=%g %s' % (input_fn, ntrees, cv_folds, max_features, r2_rmse)
-            gnuplot(title, truth, preds)
+    		model = train_class(ntrees, nprocs, msl, max_features,
+    				    X_train, y_train)
+                    if model_output_fn != '':
+                        log('saving model to %s' % model_output_fn)
+                        joblib.dump(model, model_output_fn, compress=('xz',9))
+     	# predict w/ trained model
+    	if train_p < 1.0:
+    		y_preds = test(model, X_test)
+                    dump_pred_labels(output_fn, names_test, y_preds)
+                    # train_p = 0.0: we are in production,
+                    # assume there are no labels
+                    if train_p > 0.0:
+                        auc = sklearn.metrics.roc_auc_score(y_test, y_preds)
+                        mcc = sklearn.metrics.matthews_corrcoef(y_test, y_preds)
+                        log('AUC: %.3f MCC: %.3f' % (auc, mcc))
+        else: #regression
+            X_train, y_train = read_AP_lines_regr(max_feat, train_lines)
+            X_test, y_test = read_AP_lines_regr(max_feat, test_lines)
+            if model_input_fn != '':
+                log('loading model from %s' % model_input_fn)
+                model = joblib.load(model_input_fn)
+            else:
+                model = train_regr(ntrees, crit, nprocs, msl, max_features,
+                                   max_samples, X_train, y_train)
+                if model_output_fn != '':
+                    log('saving model to %s' % model_output_fn)
+                    if no_compress:
+                        joblib.dump(model, model_output_fn, compress=False)
+                    else:
+                        joblib.dump(model, model_output_fn, compress=3)
+            # predict w/ trained model
+            if train_p < 1.0:
+                y_preds = test(model, X_test)
+                dump_pred_scores(output_fn, y_preds)
+                r2 = r2_score(y_test, y_preds)
+                rmse = mean_squared_error(y_test, y_preds, squared=False)
+                if train_p > 0.0:
+                    # train/test case
+                    log('R2: %f RMSE: %f' % (r2, rmse))
+                else:
+                    # maybe production run or predictions
+                    # on an external validation set
+                    log('R2: %f RMSE: %f !!! ONLY VALID if test set had target values !!!' % (r2, rmse))
+        else: # cv_folds > 1
+    	if classification:
+                truth, preds = train_test_NxCV_class(max_feat, ntrees, nprocs, msl,
+                                                     max_features, all_lines,
+                                                     cv_folds)
+                log('truths: %d preds: %d' % (len(truth), len(preds)))
+                auc = sklearn.metrics.roc_auc_score(truth, preds)
+                mcc = sklearn.metrics.matthews_corrcoef(truth, preds)
+                log('AUC: %.3f MCC: %.3f' % (auc, mcc))
+    	else: #regression
+                truth, preds = train_test_NxCV_regr(max_feat, ntrees, crit,
+                                                    nprocs, msl, max_features,
+                                                    max_samples, all_lines,
+                                                    cv_folds)
+                log('truths: %d preds: %d' % (len(truth), len(preds)))
+                r2 = r2_score(truth, preds)
+                rmse = mean_squared_error(truth, preds, squared=False)
+                r2_rmse = 'R2=%.3f RMSE=%.4f' % (r2, rmse)
+                log(r2_rmse)
+                title = '%s N=%d folds=%d mtry=%g %s' % (input_fn, ntrees, cv_folds, max_features, r2_rmse)
+                gnuplot(title, truth, preds)
     after = time.time()
     dt = after - before
     log('dt: %.2f' % dt)
