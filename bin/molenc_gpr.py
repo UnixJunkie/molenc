60c0a0e5002124b336f4c31b7800f2a92bc247fc
Francois Berenger <ligand@free.fr>
2024-07-19 11:48:45 +0900

    bin/molenc_gpr.py: imported Tanimoto and TanimotoGP classes

--- bin/molenc_gpr.py
+++ bin/molenc_gpr.py
@@ -7,24 +7,93 @@
 # Random Forests Regressor or Classifier (RFR or RFC) CLI wrapper
 
 import argparse
+import gpflow
+import joblib
 import math
+import numpy as np
 import os
 import random
 import scipy
 import sklearn
 import sys
 import tempfile
+import tensorflow as tf
 import time
 import typing
-import joblib
 
-from sklearn.linear_model import ElasticNet, ElasticNetCV
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import r2_score, root_mean_squared_error
+from gpflow.mean_functions import Constant
+from gpflow.utilities import positive
+from gpflow.utilities.ops import broadcasting_elementwise
 from scipy import sparse
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import ElasticNet, ElasticNetCV
+from sklearn.metrics import r2_score, root_mean_squared_error
+from sklearn.preprocessing import StandardScaler
 
 # FBR: TODO NxCV should really be parallelized...
+# FBR: get rid of RFR traces remaining in there after
+
+# original code from
+# https://github.com/Ryan-Rhys/The-Photoswitch-Dataset/blob/master/examples/gp_regression_on_molecules.ipynb
+# then refactored by Patrick Walters
+
+class Tanimoto(gpflow.kernels.Kernel):
+    def __init__(self):
+        super().__init__()
+        # constrain kernel variance value to be positive during optimization
+        self.variance = gpflow.Parameter(1.0, transform=positive())
+
+    def K(self, X, X2=None):
+        """
+        Compute the Tanimoto kernel matrix σ² * ((<x, y>) / (||x||^2 + ||y||^2 - <x, y>))
+
+        :param X: N x D array
+        :param X2: M x D array. If None, compute the N x N kernel matrix for X.
+        :return: The kernel matrix of dimension N x M
+        """
+        if X2 is None:
+            X2 = X
+
+        Xs  = tf.reduce_sum(tf.square(X),  axis=-1) # ||X||^2
+        X2s = tf.reduce_sum(tf.square(X2), axis=-1) # ||X2||^2
+        outer_product = tf.tensordot(X, X2, [[-1], [-1]])
+
+        return self.variance * outer_product / \
+            (broadcasting_elementwise(tf.add, Xs, X2s) - outer_product)
+
+    def K_diag(self, X):
+        """
+        Compute the diagonal of the N x N kernel matrix of X
+        :param X: N x D array
+        :return: N x 1 array
+        """
+        return tf.fill(tf.shape(X)[:-1], tf.squeeze(self.variance))
+
+class TanimotoGP:
+    def __init__(self, maxiter=100):
+        self.m = None
+        self.maxiter = maxiter
+        self.y_scaler = StandardScaler()
+
+    def objective_closure(self):
+        return -self.m.log_marginal_likelihood()
+
+    def fit(self, X_train, y_train):
+        y_train_scaled = self.y_scaler.fit_transform(y_train.reshape(-1, 1))
+        k = Tanimoto()
+        self.m = gpflow.models.GPR(data=(X_train.astype(np.float64), y_train_scaled),
+                                   mean_function=Constant(np.mean(y_train_scaled)),
+                                   kernel=k,
+                                   noise_variance=1)
+        opt = gpflow.optimizers.Scipy()
+        opt.minimize(self.objective_closure, self.m.trainable_variables,
+                     options=dict(maxiter=self.maxiter))
+
+    def predict(self, X_test):
+        y_pred, y_var = self.m.predict_f(X_test.astype(np.float64))
+        y_pred = self.y_scaler.inverse_transform(y_pred)
+        return y_pred.flatten(), y_var.numpy().flatten()
 
 def hour_min_sec():
     tm = time.localtime()
