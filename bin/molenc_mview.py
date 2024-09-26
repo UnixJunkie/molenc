ca9441aaec0f9e3c64b3cae033e112ef5db3ade3
Francois Berenger <berenger@edu.k.u-tokyo.ac.jp>
2024-09-26 15:24:15 +0900

    bin/molenc_mview.py: added -i and -o CLI options

--- bin/molenc_mview.py
+++ bin/molenc_mview.py
@@ -3,11 +3,8 @@
 # create an HTML table to view all molecules in a web browser
 # .smi, .sdf and .mol2 files are supported
 
-import mols2grid, os, rdkit, sys
+import argparse, mols2grid, os, rdkit, sys
 from rdkit import Chem
-
-input_fn  = sys.argv[1]
-output_fn = sys.argv[2]
 
 def sdf_read_mols(fn):
     suppl = Chem.SDMolSupplier(fn)
@@ -28,35 +25,55 @@ def smi_read_mols(fn):
     lines = open(fn, 'r').readlines()
     return [mol_of_smi_line(line) for line in lines]
 
-mols = []
-# ----- SDF -----
-if input_fn.endswith(".sdf"):
-    # with some CLI options, we could select a subset:
-    # top 50, i..j, last 50, etc.
-    mols = sdf_read_mols(input_fn)
-# ----- MOL2 -----
-elif input_fn.endswith(".mol2"):
-    print("MOL2: not supported by rdkit; converting to sdf via obabel...", file=sys.stderr)
-    input_sdf = input_fn + ".sdf"
-    cmd = "obabel %s -O %s" % (input_fn, input_sdf)
+def main():
+    # <CLI> -------------------------------------------------------------------
+    parser = argparse.ArgumentParser(
+        description = "output molecules grid as html file")
+    parser.add_argument("-i", metavar = "input_fn", dest = "input_fn",
+                        help = "input file {smi|sdf|mol2}")
+    parser.add_argument("-o", metavar = "output_fn", dest = "output_fn",
+                        help = "output file (html)")
+    # parse CLI
+    if len(sys.argv) == 1:
+        # show help in case user has no clue of what to do
+        parser.print_help(sys.stderr)
+        sys.exit(1)
+    args = parser.parse_args()
+    input_fn = args.input_fn
+    output_fn = args.output_fn
+    # </CLI> ------------------------------------------------------------------
+    mols = []
+    # ----- SDF -----
+    if input_fn.endswith(".sdf"):
+        # with some CLI options, we could select a subset:
+        # top 50, i..j, last 50, etc.
+        mols = sdf_read_mols(input_fn)
+    # ----- MOL2 -----
+    elif input_fn.endswith(".mol2"):
+        print("MOL2: not supported by rdkit; converting to sdf via obabel...", file=sys.stderr)
+        input_sdf = input_fn + ".sdf"
+        cmd = "obabel %s -O %s" % (input_fn, input_sdf)
+        print("trying: %s" % cmd, file=sys.stderr)
+        os.system(cmd)
+        mols = sdf_read_mols(input_sdf)
+    # ----- SMILES -----
+    elif input_fn.endswith(".smi"):
+        mols = smi_read_mols(input_fn)
+    else:
+        print("unsupported file type: %s" % input_fn, file=sys.stderr)
+        exit(1)
+
+    # create HTML document
+    mols2grid.save(mols,
+                   # subset=["img", "name"],
+                   # n_cols = 2, # designed, nearest_in_training_set
+                   # size = (300, 350),
+                   output=output_fn, template="static", prerender=True)
+
+    # view in browser
+    cmd = "firefox %s" % output_fn
     print("trying: %s" % cmd, file=sys.stderr)
     os.system(cmd)
-    mols = sdf_read_mols(input_sdf)
-# ----- SMILES -----
-elif input_fn.endswith(".smi"):
-    mols = smi_read_mols(input_fn)
-else:
-    print("unsupported file type: %s" % input_fn, file=sys.stderr)
-    exit(1)
 
-# create HTML document
-mols2grid.save(mols,
-               # subset=["img", "name"],
-               # n_cols = 2, # designed, nearest_in_training_set
-               # size = (300, 350),
-               output=output_fn, template="static", prerender=True)
-
-# view in browser
-cmd = "firefox %s" % output_fn
-print("trying: %s" % cmd, file=sys.stderr)
-os.system(cmd)
+if __name__ == '__main__':
+    main()
