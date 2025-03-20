3b71257303fac59ead56380086bcb9a15e789833
Francois Berenger <ligand@free.fr>
2025-03-20 17:45:17 +0000

    bin/molenc_fscan.py: proper CLI handling
    added --hetero option; to only process heteroatoms

--- bin/molenc_fscan.py
+++ bin/molenc_fscan.py
@@ -5,7 +5,7 @@
 # at a time has all of its hydrogens replaced by F
 # optionally, do this only for heteroatoms
 
-import rdkit, sys
+import argparse, rdkit, sys
 from rdkit import Chem
 
 def RobustSmilesMolSupplier(input_fn):
@@ -20,32 +20,46 @@ def RobustSmilesMolSupplier(input_fn):
 
 fluor = Chem.Atom(9)
 
-# FBR: proper CLI handling
 if __name__ == '__main__':
-    argc = len(sys.argv)
-    if argc != 2:
-        print("usage: %s input.smi [--hetero]" % sys.argv[0])
+    # CLI options parsing
+    parser = argparse.ArgumentParser(
+        description = "compute atom types and distances")
+    parser.add_argument("-i", metavar = "input.smi", dest = "input_fn",
+                        help = "molecules input file")
+    parser.add_argument("-o", metavar = "output.smi", dest = "output_fn",
+                        help = "molecules output file")
+    parser.add_argument('--hetero', dest='only_heteroatoms', action='store_true',
+                        help = "only scan heteroatoms")
+    # parse CLI
+    if len(sys.argv) == 1:
+        # show help in case user has no clue of what to do
+        parser.print_help(sys.stderr)
         sys.exit(1)
-    input_fn = sys.argv[1]
-    for smi, name in RobustSmilesMolSupplier(input_fn):
-        # output original molecule first
-        print("%s\t%s" % (smi, name))
-        mol = Chem.MolFromSmiles(smi)
-        mol = Chem.AddHs(mol)
-        # then output its variants
-        count = 1
-        for a in mol.GetAtoms():
-            if a.GetAtomicNum() > 1:
-                # heavy atom
-                if a.GetTotalNumHs(includeNeighbors=True) >= 1:
-                    # hydrogens attached
-                    editable = Chem.EditableMol(mol)
-                    for neighb in a.GetNeighbors():
-                        if neighb.GetAtomicNum() == 1:
-                            # Fluorine instead
-                            a_j = neighb.GetIdx()
-                            editable.ReplaceAtom(a_j, fluor)
-                    edited = editable.GetMol()
-                    smi = Chem.MolToSmiles(edited)
-                    print("%s\t%s_%d" % (smi, name, count))
-                    count += 1
+    args = parser.parse_args()
+    input_fn = args.input_fn
+    output_fn = args.output_fn
+    only_hetero = args.only_heteroatoms
+    with open(output_fn, 'w') as out:
+        for smi, name in RobustSmilesMolSupplier(input_fn):
+            # output original molecule first
+            print("%s\t%s" % (smi, name), file=out)
+            mol = Chem.MolFromSmiles(smi)
+            mol = Chem.AddHs(mol)
+            # then output its fluorinated analogs
+            count = 1
+            for a in mol.GetAtoms():
+                anum = a.GetAtomicNum()
+                if anum > 1 and ((not only_hetero) or anum != 6):
+                    # heavy atom
+                    if a.GetTotalNumHs(includeNeighbors=True) >= 1:
+                        # hydrogens attached
+                        editable = Chem.EditableMol(mol)
+                        for neighb in a.GetNeighbors():
+                            if neighb.GetAtomicNum() == 1:
+                                # Fluorine instead
+                                a_j = neighb.GetIdx()
+                                editable.ReplaceAtom(a_j, fluor)
+                        edited = editable.GetMol()
+                        smi = Chem.MolToSmiles(edited)
+                        print("%s\t%s_%d" % (smi, name, count), file=out)
+                        count += 1
