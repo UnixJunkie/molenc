9677c120f4259c32bf6b56e5677cea57936d502d
Francois Berenger <ligand@free.fr>
2025-08-12 17:59:03 +0100

    bin/molenc_get_tag.py: ~4 times faster on large files
    no rdkit dependency also :-)

--- bin/molenc_get_tag.py
+++ bin/molenc_get_tag.py
@@ -1,25 +1,21 @@
 #!/usr/bin/env python3
 #
-# Extract given tags from an SDF file (sdf2csv)
+# Extract given tags from an SDF file; not parsing molecules for speed
 
-import argparse, gzip, rdkit, sys
-from rdkit import Chem
+import argparse, gzip, re, sys
 
-# m.GetProp but w/ a default value
-def get_prop_default(m, prop, def_val):
-    res = def_val
-    try:
-        res = m.GetProp(prop)
-    except KeyError:
-        pass
-    return res
+class End_of_file(Exception):
+    pass
 
-def get_props(m, tags):
-    res = []
-    for t in tags:
-        x = get_prop_default(m, t, '')
-        res.append(x)
-    return res
+def open_maybe_compressed_file(fn):
+    if fn.endswith(".gz"):
+        return gzip.open(fn, 'rt')
+    else:
+        return open(fn, 'rt')
+
+# regexp to capture an SDF tag (key)
+# next line must contain the corresponding value
+sdf_tag_name = re.compile('>  <(.+)>')
 
 if __name__ == '__main__':
     # CLI options parsing
@@ -40,22 +36,42 @@ if __name__ == '__main__':
     input_fn = args.input_fn
     output_fn = args.output_fn
     tags = args.tags.strip().split(',')
+    tags_set = set(tags)
     # -------------------------------------------------------------------------
-    suppl = None
-    if input_fn.endswith(".sdf.gz"):
-        # we just want to extract SDF tags; we don't care if rdkit
-        # is unhappy w/ some molecules --> sanitize=False
-        suppl = Chem.ForwardSDMolSupplier(gzip.open(input_fn), sanitize=False)
-    else:
-        suppl = Chem.SDMolSupplier(input_fn, sanitize=False)
-    with open(output_fn, 'w') as out:
-        for mol in suppl:
-            if mol:
-                props = get_props(mol, tags)
-                for i, p in enumerate(props):
-                    if i > 0:
-                        print(',%s' % p, end='', file=out)
-                    else:
-                        print('%s' % p, end='', file=out)
-                # EOL plus makes empty fields obvious
-                print(',', file=out)
+    res: dict[str,list[str]] = {}
+    for tag in tags:
+        res[tag] = []
+    # capture all tags we are interested in
+    with open_maybe_compressed_file(input_fn) as input:
+        try:
+            while True:
+                line = input.readline()
+                if line == '':
+                    raise End_of_file
+                elif line[0] == '>':
+                    match = sdf_tag_name.match(line)
+                    if match:
+                        key = match.group(1)
+                        if key in tags_set:
+                            value: str = input.readline().strip()
+                            prev_vals = res[key]
+                            prev_vals.append(value)
+                            res[key] = prev_vals
+        except End_of_file:
+            pass
+    # check all tags of interest were seen the same number of times
+    expected = len(res[tags[0]])
+    for tag in tags:
+        if expected != len(res[tag]):
+            print('FATAL: expected: %d <> len(res[%s]) = %d' % \
+                  (expected, tag, len(res[tag])), file=sys.stderr)
+            exit(1)
+    # CSV output
+    values = []
+    for tag in tags:
+        values.append(res[tag])
+    with open(output_fn, 'w') as csv_out:
+        for i in range(expected):
+            for j in range(len(values)):
+                print("%s" % values[j][i], file=csv_out, end=',')
+            print("", file=csv_out) # EOL
