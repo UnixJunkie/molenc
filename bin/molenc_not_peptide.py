9c3c3f716633e627beb19e561a74838981e08b1c
Francois Berenger <ligand@free.fr>
2025-11-28 13:59:14 +0000

    new file:   bin/molenc_not_peptide.py

---
+++ bin/molenc_not_peptide.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+#
+# only output molecules w/o three consecutive peptide bonds
+# molecules not filtered out are written to stdout
+# usage: molenc_not_peptide.py INPUT.smi
+
+import rdkit
+from rdkit import Chem
+import sys
+
+input_fn = sys.argv[1]
+
+total = 0
+peptides = 0
+errors = 0
+
+three_pep_bonds = Chem.MolFromSmiles('NCC(=O)NCC(=O)NCC(=O)')
+
+def is_peptide(mol):
+    return mol.HasSubstructMatch(three_pep_bonds)
+
+for line in open(input_fn).readlines():
+    smi = line.strip().split()[0]
+    mol = Chem.MolFromSmiles(smi)
+    if mol:
+        if not is_peptide(mol):
+            print(line, end='')
+        else:
+            peptides += 1
+    else:
+        print('ERROR: could not parse: %s' % line, file=sys.stderr)
+        errors += 1
+    total += 1
+
+print('INFO: errors/peptides/total=%d/%d/%d' % (errors, peptides, total),
+      file=sys.stderr)
